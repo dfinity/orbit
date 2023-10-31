0b80a086a8939d46b93cc94ae7008576cfb1af2f
Kepler Vital <dev@keplervital.com>
2023-10-31 17:35:23 +0100

    test: add indexes tests

--- canisters/bank/src/models/indexes/transfer_execution_time_index.rs
+++ canisters/bank/src/models/indexes/transfer_execution_time_index.rs
@@ -1,8 +1,8 @@
+use crate::core::ic_cdk::api::time;
 use crate::models::{Transfer, TransferExecutionPlan, TransferId};
 use candid::{CandidType, Deserialize};
 use ic_canister_core::types::Timestamp;
 use ic_canister_macros::stable_object;
-use ic_cdk::api::time;
 use std::hash::Hash;
 
 /// Represents a transfer index by execution time.
@@ -30,5 +30,62 @@ impl Transfer {
             },
             transfer_id: self.id,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::{PolicySnapshot, TransferStatus};
+    use num_bigint::BigUint;
+
+    #[test]
+    fn test_transfer_to_index_by_execution_dt_scheduled() {
+        let transfer = Transfer {
+            id: [0; 16],
+            amount: candid::Nat(BigUint::from(0u32)),
+            blockchain_network: "ethereum".to_string(),
+            created_timestamp: 0,
+            expiration_dt: 0,
+            fee: candid::Nat(BigUint::from(0u32)),
+            from_wallet: [1; 16],
+            to_address: "0x1234".to_string(),
+            status: TransferStatus::Pending,
+            initiator_account: [2; 16],
+            last_modification_timestamp: 0,
+            metadata: vec![],
+            policy_snapshot: PolicySnapshot { min_approvals: 0 },
+            execution_plan: TransferExecutionPlan::Scheduled { execution_time: 2 },
+        };
+
+        let index = transfer.to_index_by_execution_dt();
+
+        assert_eq!(index.transfer_id, transfer.id);
+        assert_eq!(index.execution_dt, 2);
+    }
+
+    #[test]
+    fn test_transfer_to_index_by_execution_dt_immediate() {
+        let transfer = Transfer {
+            id: [0; 16],
+            amount: candid::Nat(BigUint::from(0u32)),
+            blockchain_network: "ethereum".to_string(),
+            created_timestamp: 0,
+            expiration_dt: 0,
+            fee: candid::Nat(BigUint::from(0u32)),
+            from_wallet: [1; 16],
+            to_address: "0x1234".to_string(),
+            status: TransferStatus::Pending,
+            initiator_account: [2; 16],
+            last_modification_timestamp: 0,
+            metadata: vec![],
+            policy_snapshot: PolicySnapshot { min_approvals: 0 },
+            execution_plan: TransferExecutionPlan::Immediate,
+        };
+
+        let index = transfer.to_index_by_execution_dt();
+
+        assert_eq!(index.transfer_id, transfer.id);
+        assert_eq!(index.execution_dt, time());
     }
 }
--- canisters/bank/src/models/indexes/wallet_account_index.rs
+++ canisters/bank/src/models/indexes/wallet_account_index.rs
@@ -27,3 +27,35 @@ impl Wallet {
             .collect::<Vec<WalletAccountIndex>>()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::{Blockchain, BlockchainStandard};
+
+    #[test]
+    fn test_wallet_to_account_association() {
+        let wallet = Wallet {
+            id: [0; 16],
+            address: "0x1234".to_string(),
+            balance: None,
+            blockchain: Blockchain::InternetComputer,
+            decimals: 0u32,
+            name: None,
+            owners: vec![[1; 16], [2; 16]],
+            policies: vec![],
+            standard: BlockchainStandard::Native,
+            last_modification_timestamp: 0,
+            metadata: vec![],
+            symbol: "ICP".to_string(),
+        };
+
+        let indexes = wallet.to_index_by_accounts();
+
+        assert_eq!(indexes.len(), 2);
+        assert_eq!(indexes[0].account_id, [1; 16]);
+        assert_eq!(indexes[0].wallet_id, [0; 16]);
+        assert_eq!(indexes[1].account_id, [2; 16]);
+        assert_eq!(indexes[1].wallet_id, [0; 16]);
+    }
+}
