c672ae35024129110e6ef0af3614478e78231c5a
Martin Raszyk <martin.raszyk@dfinity.org>
2023-11-17 19:05:42 +0100

    simplify

--- canisters/integration_tests/src/utils.rs
+++ canisters/integration_tests/src/utils.rs
@@ -1,15 +1,15 @@
 use candid::Principal;
 
-pub fn minter_test_id() -> Principal {
+pub fn controller_test_id() -> Principal {
     let mut bytes = 0_u64.to_le_bytes().to_vec();
-    bytes.push(0xfc); // internal marker for minter test id
+    bytes.push(0xfd); // internal marker for controller test id
     bytes.push(0x01); // marker for opaque ids
     Principal::from_slice(&bytes)
 }
 
-pub fn controller_test_id() -> Principal {
+pub fn minter_test_id() -> Principal {
     let mut bytes = 0_u64.to_le_bytes().to_vec();
-    bytes.push(0xfd); // internal marker for controller test id
+    bytes.push(0xfc); // internal marker for minter test id
     bytes.push(0x01); // marker for opaque ids
     Principal::from_slice(&bytes)
 }
