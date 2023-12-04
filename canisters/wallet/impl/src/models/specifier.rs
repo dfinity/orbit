b2e6cf9c8e93af529e605d3f48267bfc0032a69b
Or Ricon <rikonor@gmail.com>
2023-12-04 13:17:00 -0500

    policy specifiers

---
+++ canisters/wallet/impl/src/models/specifier.rs
@@ -0,0 +1,354 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use ic_canister_core::types::UUID;
+
+use super::{Proposal, ProposalOperation, ProposalOperationType};
+
+#[derive(Clone)]
+pub enum AccountSpecifier {
+    Any,
+    Group(Vec<UUID>),
+    Id(Vec<UUID>),
+}
+
+#[derive(Clone)]
+pub enum AddressSpecifier {
+    Any,
+}
+
+#[derive(Clone)]
+pub enum GroupSpecifier {
+    Any,
+    Name(Vec<UUID>),
+}
+
+#[derive(Clone)]
+pub enum UserSpecifier {
+    Any,
+    Group(Vec<UUID>),
+    Id(Vec<UUID>),
+    Proposer,
+}
+
+#[derive(Clone)]
+pub enum ProposalSpecifier {
+    AddAccount,
+    AddUser,
+    EditAccount(AccountSpecifier),
+    EditUser(UserSpecifier),
+    Transfer(AccountSpecifier, AddressSpecifier),
+}
+
+impl From<&ProposalSpecifier> for ProposalOperationType {
+    fn from(value: &ProposalSpecifier) -> Self {
+        match value {
+            ProposalSpecifier::AddAccount => ProposalOperationType::AddAccount,
+            ProposalSpecifier::AddUser => ProposalOperationType::AddUser,
+            ProposalSpecifier::EditAccount(_) => ProposalOperationType::EditAccount,
+            ProposalSpecifier::EditUser(_) => ProposalOperationType::EditUser,
+            ProposalSpecifier::Transfer(_, _) => ProposalOperationType::Transfer,
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum MatchError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+#[async_trait]
+pub trait Match<T>: Sync + Send {
+    async fn is_match(&self, v: T) -> Result<bool, MatchError>;
+}
+
+pub struct AccountMatcher;
+
+#[async_trait]
+impl Match<(Proposal, UUID, AccountSpecifier)> for AccountMatcher {
+    async fn is_match(&self, v: (Proposal, UUID, AccountSpecifier)) -> Result<bool, MatchError> {
+        let (_, id, s) = v;
+
+        match s {
+            // Any
+            AccountSpecifier::Any => Ok(true),
+
+            // Group
+            AccountSpecifier::Group(ids) => todo!(),
+
+            // Id
+            AccountSpecifier::Id(ids) => Ok(ids.contains(&id)),
+        }
+    }
+}
+
+pub struct AddressMatcher;
+
+#[async_trait]
+impl Match<(Proposal, String, AddressSpecifier)> for AddressMatcher {
+    async fn is_match(&self, v: (Proposal, String, AddressSpecifier)) -> Result<bool, MatchError> {
+        let (_, _, s) = v;
+
+        match s {
+            AddressSpecifier::Any => Ok(true),
+        }
+    }
+}
+
+pub struct UserMatcher;
+
+#[async_trait]
+impl Match<(Proposal, UUID, UserSpecifier)> for UserMatcher {
+    async fn is_match(&self, v: (Proposal, UUID, UserSpecifier)) -> Result<bool, MatchError> {
+        let (p, id, s) = v;
+
+        match s {
+            // Any
+            UserSpecifier::Any => Ok(true),
+
+            // Group
+            UserSpecifier::Group(ids) => todo!(),
+
+            // Id
+            UserSpecifier::Id(ids) => Ok(ids.contains(&id)),
+
+            // Proposer
+            UserSpecifier::Proposer => Ok(p.proposed_by == id),
+        }
+    }
+}
+
+pub struct ProposalMatcher {
+    account_matcher: Arc<dyn Match<(Proposal, UUID, AccountSpecifier)>>,
+    address_matcher: Arc<dyn Match<(Proposal, String, AddressSpecifier)>>,
+    user_matcher: Arc<dyn Match<(Proposal, UUID, UserSpecifier)>>,
+}
+
+#[async_trait]
+impl Match<(Proposal, ProposalSpecifier)> for ProposalMatcher {
+    async fn is_match(&self, v: (Proposal, ProposalSpecifier)) -> Result<bool, MatchError> {
+        let (p, s) = v;
+
+        Ok(match (p.operation.to_owned(), s) {
+            // AddAccount
+            (ProposalOperation::AddAccount(_), ProposalSpecifier::AddAccount) => true,
+
+            // AddUser
+            (ProposalOperation::AddUser(_), ProposalSpecifier::AddUser) => true,
+
+            // EditAccount
+            (ProposalOperation::EditAccount(params), ProposalSpecifier::EditAccount(account)) => {
+                self.account_matcher
+                    .is_match((p, params.input.account_id, account))
+                    .await?
+            }
+
+            // EditUser
+            (ProposalOperation::EditUser(params), ProposalSpecifier::EditUser(user)) => {
+                self.user_matcher
+                    .is_match((p, params.input.user_id, user))
+                    .await?
+            }
+
+            // Transfer
+            (
+                ProposalOperation::Transfer(params),
+                ProposalSpecifier::Transfer(account, address),
+            ) => vec![
+                // Account
+                self.account_matcher
+                    .is_match((p.clone(), params.input.from_account_id, account))
+                    .await?,
+                // Address
+                self.address_matcher
+                    .is_match((p.clone(), params.input.to, address))
+                    .await?,
+            ]
+            .into_iter()
+            .all(|v| v),
+
+            _ => false,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use anyhow::{anyhow, Error};
+    use candid::Nat;
+
+    use crate::models::{
+        specifier::{
+            AccountMatcher, AccountSpecifier, AddressMatcher, AddressSpecifier, Match,
+            ProposalMatcher, ProposalSpecifier, UserMatcher, UserSpecifier,
+        },
+        AddAccountOperation, AddAccountOperationInput, AddUserOperation, AddUserOperationInput,
+        Blockchain, EditAccountOperation, EditAccountOperationInput, EditUserOperation,
+        EditUserOperationInput, Proposal, ProposalExecutionPlan, ProposalOperation, ProposalStatus,
+        TransferOperation, TransferOperationInput, UserStatus,
+    };
+
+    #[tokio::test]
+    async fn test_proposal_matcher_empty_proposal() -> Result<(), Error> {
+        let m = ProposalMatcher {
+            account_matcher: Arc::new(AccountMatcher),
+            address_matcher: Arc::new(AddressMatcher),
+            user_matcher: Arc::new(UserMatcher),
+        };
+
+        let tcs = vec![
+            (
+                ProposalOperation::AddAccount(AddAccountOperation {
+                    account_id: None,
+                    input: AddAccountOperationInput {
+                        name: "account-1".into(),
+                        owners: vec![],
+                        policies: vec![],
+                        blockchain: Blockchain::InternetComputer,
+                        standard: crate::models::BlockchainStandard::Native,
+                        metadata: vec![],
+                    },
+                }),
+                ProposalSpecifier::AddAccount,
+            ),
+            (
+                ProposalOperation::AddUser(AddUserOperation {
+                    user_id: None,
+                    input: AddUserOperationInput {
+                        name: None,
+                        identities: vec![],
+                        groups: vec![],
+                        status: UserStatus::Active,
+                    },
+                }),
+                ProposalSpecifier::AddUser,
+            ),
+            (
+                ProposalOperation::EditAccount(EditAccountOperation {
+                    input: EditAccountOperationInput {
+                        account_id: [0; 16],
+                        owners: None,
+                        policies: None,
+                        name: None,
+                    },
+                }),
+                ProposalSpecifier::EditAccount(AccountSpecifier::Any),
+            ),
+            (
+                ProposalOperation::EditUser(EditUserOperation {
+                    input: EditUserOperationInput {
+                        user_id: [0; 16],
+                        name: None,
+                        identities: None,
+                        groups: None,
+                    },
+                }),
+                ProposalSpecifier::EditUser(UserSpecifier::Any),
+            ),
+            (
+                ProposalOperation::Transfer(TransferOperation {
+                    transfer_id: None,
+                    input: TransferOperationInput {
+                        from_account_id: [0; 16],
+                        to: "address-1".into(),
+                        amount: Nat::from(1),
+                        metadata: vec![],
+                        network: "network-1".into(),
+                        fee: None,
+                    },
+                }),
+                ProposalSpecifier::Transfer(AccountSpecifier::Any, AddressSpecifier::Any),
+            ),
+        ];
+
+        for tc in tcs {
+            // Proposal
+            let p = Proposal {
+                id: [0; 16],
+                title: "title".into(),
+                summary: None,
+                proposed_by: [0; 16],
+                status: ProposalStatus::Created,
+                operation: tc.0,
+                expiration_dt: 0,
+                execution_plan: ProposalExecutionPlan::Immediate,
+                votes: vec![],
+                metadata: vec![],
+                created_timestamp: 0,
+                last_modification_timestamp: 0,
+            };
+
+            // Specifier
+            let s = tc.1;
+
+            if !m.is_match((p, s)).await? {
+                return Err(anyhow!("expected true but got false"));
+            }
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_user_matcher() -> Result<(), Error> {
+        let m = UserMatcher;
+
+        let tcs = vec![
+            (
+                [0; 16],            // proposer
+                [1; 16],            // voter
+                UserSpecifier::Any, // specifier
+            ),
+            (
+                [0; 16],                          // proposer
+                [1; 16],                          // voter
+                UserSpecifier::Id(vec![[1; 16]]), // specifier
+            ),
+            (
+                [0; 16],                 // proposer
+                [0; 16],                 // voter
+                UserSpecifier::Proposer, // specifier
+            ),
+        ];
+
+        for tc in tcs {
+            // Proposal
+            let p = Proposal {
+                id: [0; 16],
+                title: "title".into(),
+                summary: None,
+                proposed_by: tc.0,
+                status: ProposalStatus::Created,
+                operation: ProposalOperation::EditAccount(EditAccountOperation {
+                    input: EditAccountOperationInput {
+                        account_id: [0; 16],
+                        owners: None,
+                        policies: None,
+                        name: None,
+                    },
+                }),
+                expiration_dt: 0,
+                execution_plan: ProposalExecutionPlan::Immediate,
+                votes: vec![],
+                metadata: vec![],
+                created_timestamp: 0,
+                last_modification_timestamp: 0,
+            };
+
+            // Voter
+            let u = tc.1;
+
+            // Specifier
+            let s = tc.2;
+
+            if !m.is_match((p, u, s)).await? {
+                return Err(anyhow!("expected true but got false"));
+            };
+        }
+
+        Ok(())
+    }
+}
