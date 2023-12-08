c7a7e59e9f560dd430204c42831ed1ed2c7793a3
Or Ricon <rikonor@gmail.com>
2023-12-08 09:16:05 -0500

    stop/start canister prior to upgrade

--- canisters/wallet/impl/src/services/upgrade.rs
+++ canisters/wallet/impl/src/services/upgrade.rs
@@ -1,8 +1,9 @@
 use anyhow::anyhow;
-use candid::{CandidType, Principal};
+use candid::{CandidType, Encode, Principal};
 use ic_canister_core::api::{ApiError, ServiceResult};
-use ic_cdk::api::management_canister::main::{
-    self as mgmt, CanisterInstallMode, InstallCodeArgument,
+use ic_cdk::api::management_canister::{
+    main::{self as mgmt, CanisterInstallMode, InstallCodeArgument},
+    provisional::CanisterIdRecord,
 };
 use lazy_static::lazy_static;
 
@@ -56,15 +57,41 @@ impl UpgradeService {
             )),
         }?;
 
-        mgmt::install_code(InstallCodeArgument {
+        // Stop canister
+        let stop_result = mgmt::stop_canister(CanisterIdRecord {
+            canister_id: upgrader_canister_id.to_owned(),
+        })
+        .await
+        .map_err(|(_, err)| ApiError::new("UPGRADE_FAILED".to_string(), Some(err), None));
+
+        if stop_result.is_err() {
+            // Restart canister if the stop did not succeed (its possible the canister did stop running)
+            mgmt::start_canister(CanisterIdRecord {
+                canister_id: upgrader_canister_id.to_owned(),
+            })
+            .await
+            .map_err(|(_, err)| ApiError::new("UPGRADE_FAILED".to_string(), Some(err), None))?;
+
+            return stop_result;
+        }
+
+        // Upgrade canister
+        let upgrade_result = mgmt::install_code(InstallCodeArgument {
             mode: CanisterInstallMode::Upgrade,
-            canister_id: upgrader_canister_id,
+            canister_id: upgrader_canister_id.to_owned(),
             wasm_module: module.to_owned(),
             arg: vec![],
         })
         .await
+        .map_err(|(_, err)| ApiError::new("UPGRADE_FAILED".to_string(), Some(err), None));
+
+        // Restart canister (regardless of whether the upgrade succeeded or not)
+        mgmt::start_canister(CanisterIdRecord {
+            canister_id: upgrader_canister_id.to_owned(),
+        })
+        .await
         .map_err(|(_, err)| ApiError::new("UPGRADE_FAILED".to_string(), Some(err), None))?;
 
-        Ok(())
+        upgrade_result
     }
 }
