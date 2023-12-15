712dbda467eb078e5e7a4abb0aff6b441db16082
Or Ricon <rikonor@gmail.com>
2023-12-15 10:57:38 -0500

    add basic metrics to wallet

---
+++ canisters/wallet/impl/src/core/metrics.rs
@@ -0,0 +1,25 @@
+use std::cell::RefCell;
+
+use prometheus::{Gauge, Registry};
+
+const SERVICE_NAME: &str = "wallet";
+
+thread_local! {
+    pub static GAUGE_CANISTER_CYCLES_BALANCE: RefCell<Gauge> = RefCell::new({
+        Gauge::new(
+            format!("{SERVICE_NAME}_canister_cycles_balance"), // name
+            "cycles balance available to the canister", // help
+        ).unwrap()
+    });
+
+    pub static METRICS_REGISTRY: RefCell<Registry> = RefCell::new({
+        let r = Registry::new();
+
+        GAUGE_CANISTER_CYCLES_BALANCE.with(|g| {
+            let g = Box::new(g.borrow().to_owned());
+            r.register(g).unwrap();
+        });
+
+        r
+    });
+}
--- canisters/wallet/impl/src/core/mod.rs
+++ canisters/wallet/impl/src/core/mod.rs
@@ -14,6 +14,8 @@ pub use call_context::*;
 
 pub mod middlewares;
 
+pub mod metrics;
+
 #[cfg(not(test))]
 pub use ic_canister_core::cdk as ic_cdk;
 #[cfg(test)]
