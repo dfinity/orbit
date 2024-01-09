3fe9c59204ed29797cac65572cea3668397f74ec
Martin Raszyk <martin.raszyk@dfinity.org>
2024-01-09 10:01:09 +0100

    feat: address book entry type

---
+++ canisters/wallet/api/src/address_book.rs
@@ -0,0 +1,19 @@
+use crate::{UuidDTO};
+use candid::{CandidType, Deserialize};
+
+#[derive(CandidType, Deserialize, Debug, Clone)]
+pub struct AddressBookMetadataDTO {
+    pub key: String,
+    pub value: String,
+}
+
+#[derive(CandidType, Deserialize, Debug, Clone)]
+pub struct AddressBookEntryDTO {
+    pub id: UuidDTO,
+    pub owner: Option<String>,
+    pub address: String,
+    pub blockchain: String,
+    pub standard: String,
+    pub metadata: Vec<AddressBookMetadataDTO>,
+    pub last_modification_timestamp: String,
+}
