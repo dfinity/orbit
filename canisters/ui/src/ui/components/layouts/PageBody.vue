a3a87af5c3b35b7731b0bafa367de042deabdcbf
Kepler Vital <dev@keplervital.com>
2024-01-17 16:37:21 +0100

    chore: add page layout components (#99)

---
+++ canisters/ui/src/ui/components/layouts/PageBody.vue
@@ -0,0 +1,9 @@
+<template>
+  <VContainer class="px-8 py-4" fluid>
+    <VRow>
+      <VCol cols="12">
+        <slot></slot>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
---
+++ canisters/ui/src/ui/components/layouts/PageHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <VContainer class="pt-8 pb-0 pl-8 pr-8" fluid>
+    <VRow>
+      <VCol cols="12" :md="hasActions ? 6 : 12">
+        <h1 class="text-h4">
+          <slot name="title">{{ props.title ?? '' }}</slot>
+        </h1>
+      </VCol>
+      <VCol v-if="hasActions" cols="12" md="6" class="d-flex justify-end">
+        <slot name="actions"></slot>
+      </VCol>
+      <VCol cols="12">
+        <VDivider class="border-opacity-50 pb-2" :thickness="2" />
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
+
+<script lang="ts" setup>
+import { useSlots, computed } from 'vue';
+
+const props = defineProps<{
+  title?: string;
+}>();
+
+const slots = useSlots();
+
+const hasActions = computed(() => !!slots.actions);
+</script>
