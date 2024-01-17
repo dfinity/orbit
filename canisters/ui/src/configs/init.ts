264e226b008063f5d18cbdbd5e608863d4a91752
Kepler Vital <dev@keplervital.com>
2024-01-17 18:52:33 +0100

    fix init when canister_ids are not available

--- canisters/ui/src/configs/init.ts
+++ canisters/ui/src/configs/init.ts
@@ -16,10 +16,11 @@ const appInitConfig: AppInitConfig = {
     internetIdentity: import.meta.env.APP_PROVIDER_URL_INTERNET_IDENTITY,
   },
   canisters: {
-    controlPanel: import.meta.env.APP_CANISTER_ID_CONTROL_PANEL,
-    internetIdentity: import.meta.env.APP_CANISTER_ID_INTERNET_IDENTITY,
-    ui: import.meta.env.APP_CANISTER_ID_UI,
-    icpIndex: import.meta.env.APP_CANISTER_ID_ICP_INDEX,
+    controlPanel: import.meta.env.APP_CANISTER_ID_CONTROL_PANEL || '65evf-oqaaa-aaaal-add6q-cai',
+    ui: import.meta.env.APP_CANISTER_ID_UI || '6uh6z-yyaaa-aaaal-add7a-cai',
+    internetIdentity:
+      import.meta.env.APP_CANISTER_ID_INTERNET_IDENTITY || 'rdmx6-jaaaa-aaaaa-aaadq-cai',
+    icpIndex: import.meta.env.APP_CANISTER_ID_ICP_INDEX || 'qhbym-qaaaa-aaaaa-aaafq-cai',
   },
 };
 
