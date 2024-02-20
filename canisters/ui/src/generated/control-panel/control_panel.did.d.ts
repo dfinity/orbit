131a1e179f1a6a09b30941d0281c6c7f40074a0f
Kepler Vital <dev@keplervital.com>
2024-02-20 19:37:32 +0100

    fix dfx for ci validation job

--- canisters/ui/src/generated/control-panel/control_panel.did.d.ts
+++ canisters/ui/src/generated/control-panel/control_panel.did.d.ts
@@ -1,5 +1,6 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
 
 export interface ApiError {
   'code' : string,
@@ -67,3 +68,5 @@ export interface _SERVICE {
   'manage_user' : ActorMethod<[ManageUserInput], ManageUserResult>,
   'register_user' : ActorMethod<[RegisterUserInput], RegisterUserResult>,
 }
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: ({ IDL }: { IDL: IDL }) => IDL.Type[];
