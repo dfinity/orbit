3a61b080a27f97d1120a60ed0c37d98ba9b16fbb
Kepler Vital <dev@keplervital.com>
2024-02-21 12:08:46 +0100

    use time from core

--- canisters/wallet/impl/src/services/account.rs
+++ canisters/wallet/impl/src/services/account.rs
@@ -2,6 +2,7 @@ use crate::{
     core::{
         access_control::evaluate_caller_access,
         generate_uuid_v4,
+        ic_cdk::api::time,
         utils::{paginated_items, PaginatedData, PaginatedItemsArgs},
         CallContext, ACCOUNT_BALANCE_FRESHNESS_IN_MS,
     },
@@ -22,7 +23,7 @@ use crate::{
 };
 use futures::{stream, StreamExt};
 use ic_canister_core::{
-    api::ServiceResult, cdk::api::time, model::ModelValidator, repository::Repository, types::UUID,
+    api::ServiceResult, model::ModelValidator, repository::Repository, types::UUID,
 };
 use lazy_static::lazy_static;
 use std::sync::Arc;
