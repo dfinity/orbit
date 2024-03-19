9a410734b50c2b63a3d884262ab6b11d9c964acb
mraszyk <31483726+mraszyk@users.noreply.github.com>
2024-03-19 12:26:56 +0100

    feat: restrict the maximum number of deployed wallets (#165)
    This PR restricts the maximum number of wallets ever deployed through
    the control panel by a single user.

--- canisters/integration-tests/src/control_panel_tests.rs
+++ canisters/integration-tests/src/control_panel_tests.rs
@@ -2,7 +2,8 @@ use crate::setup::setup_new_env;
 use crate::utils::user_test_id;
 use crate::TestEnv;
 use control_panel_api::{
-    DeployWalletResponse, GetMainWalletResponse, RegisterUserInput, RegisterUserResponse,
+    DeployWalletResponse, GetMainWalletResponse, ManageUserInput, ManageUserResponse,
+    RegisterUserInput, RegisterUserResponse, UserWalletDTO,
 };
 use ic_canister_core::api::ApiResult;
 use pocket_ic::update_candid_as;
@@ -132,4 +133,73 @@ fn deploy_user_wallet() {
     .unwrap();
     let health_status = res.0;
     assert_eq!(health_status, HealthStatus::Healthy);
+}
+
+#[test]
+fn deploy_too_many_wallets() {
+    let TestEnv {
+        env, canister_ids, ..
+    } = setup_new_env();
+
+    let user_id = user_test_id(0);
+
+    // register user
+    let register_args = RegisterUserInput {
+        wallet_id: None,
+        email: Some("john@example.com".to_string()),
+    };
+    let res: (ApiResult<RegisterUserResponse>,) = update_candid_as(
+        &env,
+        canister_ids.control_panel,
+        user_id,
+        "register_user",
+        (register_args,),
+    )
+    .unwrap();
+    let user_dto = res.0.unwrap().user;
+    assert_eq!(user_dto.id, user_id);
+
+    // deploy the maximum amount of user wallets
+    let mut wallets = vec![];
+    for _ in 0..10 {
+        let res: (ApiResult<DeployWalletResponse>,) = update_candid_as(
+            &env,
+            canister_ids.control_panel,
+            user_id,
+            "deploy_wallet",
+            (),
+        )
+        .unwrap();
+        wallets.push(res.0.unwrap().canister_id);
+    }
+
+    // reset all but one deployed wallet
+    let manage_user_args = ManageUserInput {
+        main_wallet: Some(wallets[0]),
+        wallets: Some(vec![UserWalletDTO {
+            canister_id: wallets[0],
+            name: None,
+        }]),
+    };
+    let res: (ApiResult<ManageUserResponse>,) = update_candid_as(
+        &env,
+        canister_ids.control_panel,
+        user_id,
+        "manage_user",
+        (manage_user_args,),
+    )
+    .unwrap();
+    let user_dto = res.0.unwrap().user;
+    assert_eq!(user_dto.wallets.len(), 1);
+
+    // deploying an additional wallet should fail nonetheless
+    let res: (ApiResult<DeployWalletResponse>,) = update_candid_as(
+        &env,
+        canister_ids.control_panel,
+        user_id,
+        "deploy_wallet",
+        (),
+    )
+    .unwrap();
+    assert_eq!(res.0.unwrap_err().code, "DEPLOY_WALLET_QUOTA_EXCEEDED");
 }
