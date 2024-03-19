9c7b18e83033c0006e55a0dc4913a36fc318691c
Kepler Vital <dev@keplervital.com>
2024-03-19 11:51:37 +0100

    feat: uniquely creates time in the same round

--- libs/ic-canister-core/src/cdk.rs
+++ libs/ic-canister-core/src/cdk.rs
@@ -1,6 +1,19 @@
 #[cfg(not(test))]
 pub use ic_cdk::*;
 
+#[cfg(not(test))]
+pub mod ic_cdk_extended {
+    /// Returns the current time in nanoseconds since the epoch.
+    ///
+    /// This function increments the time by 1ns for each call in the same round.
+    pub fn next_time() -> u64 {
+        crate::utils::next_time(ic_cdk::api::time())
+    }
+}
+
+#[cfg(not(test))]
+pub use ic_cdk_extended::*;
+
 #[cfg(test)]
 pub use mocks::*;
 
@@ -17,6 +30,14 @@ pub mod mocks {
 
     pub fn spawn<F: 'static + std::future::Future<Output = ()>>(_future: F) {
         // do nothing since this is a mock for testing purposes
+    }
+
+    pub fn next_time() -> u64 {
+        // removes the ns precision of the current time to simulate a round time in nanoseconds
+        let round_duration_ns = crate::utils::MAX_TIME_INCREMENT_NS;
+        let round_time_ns = api::time() / round_duration_ns * round_duration_ns;
+
+        crate::utils::next_time(round_time_ns)
     }
 
     pub mod api {
@@ -65,7 +86,10 @@ pub mod mocks {
 
 #[cfg(test)]
 mod tests {
-    use super::mocks::{api::trap, caller};
+    use super::mocks::{
+        api::{set_mock_ic_time, trap},
+        caller, next_time,
+    };
     use candid::Principal;
 
     #[test]
@@ -77,5 +101,26 @@ mod tests {
     #[should_panic(expected = "this is an expected panic")]
     fn trap_panics_with_given_message() {
         trap("this is an expected panic")
+    }
+
+    #[test]
+    fn next_time_correctly_increments() {
+        set_mock_ic_time(std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_millis(100));
+
+        // increments time by 1ns for each call in the same round
+        assert_eq!(next_time(), 100_000_000);
+        assert_eq!(next_time(), 100_000_001);
+        assert_eq!(next_time(), 100_000_002);
+
+        set_mock_ic_time(std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_millis(150));
+
+        assert_eq!(next_time(), 150_000_000);
+        assert_eq!(next_time(), 150_000_001);
+        assert_eq!(next_time(), 150_000_002);
+
+        // still same round, so increment continues. Rounds are simulated to last 50ms.
+        set_mock_ic_time(std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_millis(175));
+        assert_eq!(next_time(), 150_000_003);
+        assert_eq!(next_time(), 150_000_004);
     }
 }
--- libs/ic-canister-core/src/utils/time.rs
+++ libs/ic-canister-core/src/utils/time.rs
@@ -1,4 +1,5 @@
 use crate::types::Timestamp;
+use std::cell::RefCell;
 use time::{format_description::well_known::Rfc3339, OffsetDateTime};
 
 pub fn timestamp_to_rfc3339(nanoseconds_since_epoch: &Timestamp) -> String {
@@ -17,4 +18,83 @@ pub fn rfc3339_to_timestamp(rfc3339: &str) -> Timestamp {
         .unix_timestamp_nanos();
 
     datetime as Timestamp
+}
+
+thread_local! {
+    static SAVED_TIME: RefCell<u64> = RefCell::new(0);
+    static TIME_INCREMENT: RefCell<u64> = RefCell::new(0);
+}
+
+// The maximum time increment that can be added to the current time. This allows time to be incremented by at
+// most 50ms in a single round, which means 50M calls to `time` can be made in a single round.
+pub const MAX_TIME_INCREMENT_NS: u64 = 50_000_000;
+
+/// This function increments the time by 1ns for each call in the same round.
+///
+/// Panics if the time increment exceeds the maximum value for the current round (50ms).
+pub fn next_time(ic_round_time: u64) -> u64 {
+    SAVED_TIME.with(|saved_time| {
+        let mut saved_time = saved_time.borrow_mut();
+
+        if ic_round_time != *saved_time {
+            *saved_time = ic_round_time;
+            TIME_INCREMENT.with(|time_increment| {
+                *time_increment.borrow_mut() = 0;
+            });
+
+            ic_round_time
+        } else {
+            let increment = TIME_INCREMENT.with(|time_increment| {
+                *time_increment.borrow_mut() += 1;
+
+                *time_increment.borrow()
+            });
+
+            if increment > MAX_TIME_INCREMENT_NS {
+                ic_cdk::api::trap(
+                    "Time increment has exceeded the maximum value for the current round.",
+                );
+            }
+
+            ic_round_time + increment
+        }
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn converts_timestamp_to_rfc3339() {
+        let timestamp = 1_710_843_144_770_000_000;
+        let rfc3339 = timestamp_to_rfc3339(&timestamp);
+        assert_eq!(rfc3339, "2024-03-19T10:12:24.77Z");
+    }
+
+    #[test]
+    fn converts_rfc3339_to_timestamp() {
+        let rfc3339 = "2024-03-19T10:12:24.77Z";
+        let timestamp = rfc3339_to_timestamp(rfc3339);
+        assert_eq!(timestamp, 1_710_843_144_770_000_000);
+    }
+
+    #[test]
+    fn time_increments_correctly() {
+        // The first call to `time` should return the current round time.
+        assert_eq!(next_time(1), 1);
+
+        // Following calls with the same round time should be incremented by 1.
+        assert_eq!(next_time(1), 2);
+        assert_eq!(next_time(1), 3);
+        assert_eq!(next_time(1), 4);
+
+        // Advancing to the next round should reset the time.
+        assert_eq!(next_time(10), 10);
+
+        // Following calls with the new round time should be incremented by 1.
+        assert_eq!(next_time(10), 11);
+        assert_eq!(next_time(10), 12);
+        assert_eq!(next_time(10), 13);
+    }
 }
