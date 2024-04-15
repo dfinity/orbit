5123297faafc86e5e2a353903e714b687eccb31f
Kepler Vital <dev@keplervital.com>
2024-04-15 22:21:19 +0200

    update did bindings

--- canisters/ui/src/generated/wallet/wallet.did.d.ts
+++ canisters/ui/src/generated/wallet/wallet.did.d.ts
@@ -854,4 +854,4 @@ export interface _SERVICE {
   'vote_on_proposal' : ActorMethod<[VoteOnProposalInput], VoteOnProposalResult>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
-export declare const init: ({ IDL }: { IDL: IDL }) => IDL.Type[];
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
