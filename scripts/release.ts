2474edca803ab1964f8c45d9a2a7295d00f8eb55
Kepler Vital <dev@keplervital.com>
2024-05-06 18:21:32 +0200

    add first release option

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,5 +1,17 @@
 import { releaseChangelog, releaseVersion } from 'nx/release';
 import { program } from 'commander';
+import { execSync } from 'child_process';
+
+// Function to get the first commit of a Git repository synchronously,
+// throwing an error if the command fails.
+function getFirstRepositoryCommit(): string {
+  try {
+    const stdout = execSync('git rev-list --max-parents=0 HEAD --reverse | head -1');
+    return stdout.toString().trim();
+  } catch (error) {
+    throw new Error(`Failed to get the first commit: ${error.message}`);
+  }
+}
 
 program
   .description('Release a new version of projects in the workspace')
@@ -13,11 +25,16 @@ program
       firstRelease: options.firstRelease,
     });
 
+    // If this is the first release, we don't have a previous version to compare against
+    // so we need to get the very first git commit in the history of the repository.
+    const from = options.firstRelease ? getFirstRepositoryCommit() : undefined;
+
     await releaseChangelog({
       versionData: projectsVersionData,
       version: workspaceVersion,
       dryRun: options.dryRun,
       verbose: options.verbose,
+      from,
     });
   })
   .parse();
