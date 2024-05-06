db1e00843904401b75b84ab71df039d2ec427e25
Kepler Vital <dev@keplervital.com>
2024-05-06 18:00:41 +0200

    update release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -3,29 +3,21 @@ import { program } from 'commander';
 
 program
   .description('Release a new version of projects in the workspace')
-  .option(
-    '-d, --dry-run',
-    'Whether or not to perform a dry-run of the release process, defaults to true',
-    true,
-  )
-  .option('-v, --verbose', 'Whether or not to log verbose output, defaults to false', false)
-  .option('-f, --first-release', 'Whether or not this is the first release of the project', false)
+  .option('-d, --dry-run', 'Whether or not to perform a dry-run of the release process')
+  .option('-v, --verbose', 'Whether or not to log verbose output')
+  .option('-f, --first-release', 'Whether or not this is the first release of the project')
   .action(async options => {
-    console.log('options', options);
     const { workspaceVersion, projectsVersionData } = await releaseVersion({
       dryRun: options.dryRun,
       verbose: options.verbose,
       firstRelease: options.firstRelease,
     });
 
-    console.log('workspaceVersion', workspaceVersion);
-    console.log('projectsVersionData', projectsVersionData);
-
-    // await releaseChangelog({
-    //   versionData: projectsVersionData,
-    //   version: workspaceVersion,
-    //   dryRun: options.dryRun,
-    //   verbose: options.verbose,
-    // });
+    await releaseChangelog({
+      versionData: projectsVersionData,
+      version: workspaceVersion,
+      dryRun: options.dryRun,
+      verbose: options.verbose,
+    });
   })
   .parse();
