369ca3fd2e73f4012b68a7c55570f495ecacddf7
Kepler Vital <dev@keplervital.com>
2024-05-08 11:17:54 +0200

    enable the usage of a release mode

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,6 +1,24 @@
 import { releaseChangelog, releaseVersion } from 'nx/release';
 import { program } from 'commander';
 
+// Determine the type of pre-release version to use if applicable, falling back to 'prod' if not provided
+const parseReleaseMode = (releaseMode?: string): 'alpha' | 'beta' | 'rc' | 'prod' => {
+  if (!releaseMode) {
+    return 'prod';
+  }
+
+  switch (releaseMode) {
+    case 'alpha':
+      return 'alpha';
+    case 'beta':
+      return 'beta';
+    case 'rc':
+      return 'rc';
+    default:
+      throw new Error(`Invalid pre-release mode: ${releaseMode}`);
+  }
+};
+
 program
   .description('Release a new version of projects in the workspace')
   .option(
@@ -10,16 +28,21 @@ program
   .option('-d, --dry-run', 'Whether or not to perform a dry-run of the release process')
   .option('-v, --verbose', 'Whether or not to log verbose output')
   .option('-f, --first-release', 'Whether or not this is the first release of the project')
+  .option('-P, --pre-release', 'Specify the type of pre-release version to use (alpha, beta or rc)')
   .action(async options => {
     const projects = options.projects
       ? options.projects.split(',').map((group: string) => group.trim())
       : undefined;
+
+    const releaseMode = parseReleaseMode(options.preRelease);
 
     const { workspaceVersion, projectsVersionData } = await releaseVersion({
       dryRun: options.dryRun,
       verbose: options.verbose,
       firstRelease: options.firstRelease,
       projects,
+      specifier: releaseMode !== 'prod' ? 'prerelease' : undefined,
+      preid: releaseMode !== 'prod' ? releaseMode : undefined,
     });
 
     await releaseChangelog({
