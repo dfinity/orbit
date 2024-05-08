291c32f01704ab65441723eb2031667a3d1cb41f
Kepler Vital <dev@keplervital.com>
2024-05-08 11:24:36 +0200

    add args parse

--- scripts/release.ts
+++ scripts/release.ts
@@ -2,7 +2,7 @@ import { releaseChangelog, releaseVersion } from 'nx/release';
 import { program } from 'commander';
 
 // Determine the type of pre-release version to use if applicable, falling back to 'prod' if not provided
-const parseReleaseMode = (releaseMode?: string): 'alpha' | 'beta' | 'rc' | 'prod' => {
+const parsePreReleaseMode = (releaseMode?: string): 'alpha' | 'beta' | 'rc' | 'prod' => {
   if (!releaseMode) {
     return 'prod';
   }
@@ -19,30 +19,37 @@ const parseReleaseMode = (releaseMode?: string): 'alpha' | 'beta' | 'rc' | 'prod
   }
 };
 
+const parseProjects = (projects?: string): string[] | undefined => {
+  if (!projects) {
+    return undefined;
+  }
+
+  return projects.split(',').map((group: string) => group.trim());
+};
+
 program
   .description('Release a new version of projects in the workspace')
   .option(
-    '-p, --projects',
+    '-p, --projects <VALUE>',
     'The release projects to include in the release, separated by commas. If not provided, all projects will be included.',
+    parseProjects,
   )
   .option('-d, --dry-run', 'Whether or not to perform a dry-run of the release process')
   .option('-v, --verbose', 'Whether or not to log verbose output')
   .option('-f, --first-release', 'Whether or not this is the first release of the project')
-  .option('-P, --pre-release', 'Specify the type of pre-release version to use (alpha, beta or rc)')
+  .option(
+    '-P, --pre-release <VALUE>',
+    'Specify the type of pre-release version to use (alpha, beta or rc)',
+    parsePreReleaseMode,
+  )
   .action(async options => {
-    const projects = options.projects
-      ? options.projects.split(',').map((group: string) => group.trim())
-      : undefined;
-
-    const releaseMode = parseReleaseMode(options.preRelease);
-
     const { workspaceVersion, projectsVersionData } = await releaseVersion({
       dryRun: options.dryRun,
       verbose: options.verbose,
       firstRelease: options.firstRelease,
-      projects,
-      specifier: releaseMode !== 'prod' ? 'prerelease' : undefined,
-      preid: releaseMode !== 'prod' ? releaseMode : undefined,
+      projects: options.projects,
+      specifier: options.preRelease !== 'prod' ? 'prerelease' : undefined,
+      preid: options.preRelease !== 'prod' ? options.preRelease : undefined,
     });
 
     await releaseChangelog({
@@ -51,7 +58,7 @@ program
       dryRun: options.dryRun,
       verbose: options.verbose,
       firstRelease: options.firstRelease,
-      projects,
+      projects: options.projects,
     });
   })
   .parse();
