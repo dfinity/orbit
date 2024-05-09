27c2a0b50e5ec988a4fa7dfefcd2170fef0be5af
olaszakos <olaszakos@gmail.com>
2024-05-09 17:30:38 +0200

    rework scheduler + strategy

--- core/station/impl/src/jobs/cancel_expired_requests.rs
+++ core/station/impl/src/jobs/cancel_expired_requests.rs
@@ -1,12 +1,26 @@
+use std::{
+    cell::RefCell,
+    collections::{BTreeMap, HashSet},
+};
+
 use crate::{
     core::ic_cdk::next_time,
-    models::{RequestStatus, RequestStatusCode},
+    models::{RequestId, RequestStatus, RequestStatusCode},
     repositories::RequestRepository,
 };
 use async_trait::async_trait;
-use orbit_essentials::repository::Repository;
+use orbit_essentials::{cdk::api::time, repository::Repository};
 
-use super::ScheduledJob;
+use super::{
+    scheduler::{ScheduleStrategy, Scheduler},
+    ScheduledJob,
+};
+
+thread_local! {
+    static TIMERS: RefCell<BTreeMap<u64, HashSet<RequestId>>> = const { RefCell::new(BTreeMap::new()) };
+    static REQUEST_MAP: RefCell<BTreeMap<RequestId, u64>> = const { RefCell::new(BTreeMap::new()) };
+    static IS_RUNNING : RefCell<bool> = const { RefCell::new(false) };
+}
 
 #[derive(Debug, Default)]
 pub struct Job {
@@ -44,5 +58,100 @@ impl Job {
         }
 
         true
+    }
+}
+
+pub struct ExpiredRequestScheduleStrategy {
+    request_id: RequestId,
+}
+
+impl ScheduleStrategy for ExpiredRequestScheduleStrategy {
+    const TOLERANCE_SEC: u64 = 5;
+
+    fn add_timer(&self, at_ns: u64) -> Option<u64> {
+        // remove the request's expiration timer if it existed
+        self.remove_timer(at_ns);
+
+        // add the timer
+        let with_tolerance = at_ns + Self::TOLERANCE_SEC * 1_000_000_000;
+        let timer_at = TIMERS.with(|timers| {
+            let mut timers = timers.borrow_mut();
+
+            // see if there is a timer around the same time
+            let existing_timer = timers
+                .range(at_ns..with_tolerance)
+                .next()
+                .map(|(timer_at, _)| *timer_at)
+                .unwrap_or(at_ns);
+
+            // if there is a timer around the same time, add the request to the existing timer
+            timers
+                .entry(existing_timer)
+                .or_default()
+                .insert(self.request_id);
+
+            existing_timer
+        });
+
+        REQUEST_MAP.with(|request_map| {
+            request_map.borrow_mut().insert(self.request_id, timer_at);
+        });
+
+        None
+    }
+
+    fn remove_timer(&self, _at_ns: u64) -> Option<u64> {
+        REQUEST_MAP.with(|request_map| {
+            let mut request_map = request_map.borrow_mut();
+            if let Some(at_ns) = request_map.remove(&self.request_id) {
+                TIMERS.with(|timers| {
+                    if let Some(request_ids) = timers.borrow_mut().get_mut(&at_ns) {
+                        request_ids.remove(&self.request_id);
+                        if request_ids.is_empty() {
+                            timers.borrow_mut().remove(&at_ns);
+
+                            Some(at_ns)
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                })
+            } else {
+                None
+            }
+        })
+    }
+
+    fn is_running(&self) -> bool {
+        IS_RUNNING.with(|is_running| *is_running.borrow())
+    }
+
+    fn set_running(&self, running: bool) {
+        IS_RUNNING.with(|is_running| *is_running.borrow_mut() = running);
+    }
+}
+
+pub fn schedule_expiration(request_id: &RequestId, at_ns: u64) {
+    let strategy = ExpiredRequestScheduleStrategy {
+        request_id: request_id.to_owned(),
+    };
+
+    Scheduler::schedule::<ExpiredRequestScheduleStrategy, Job>(
+        strategy,
+        at_ns.saturating_sub(time()),
+    );
+}
+
+pub fn cancel_scheduled_expiration(request_id: &RequestId) {
+    if let Some(at_ns) =
+        REQUEST_MAP.with(|request_map| request_map.borrow().get(request_id).copied())
+    {
+        let strategy = ExpiredRequestScheduleStrategy {
+            request_id: request_id.to_owned(),
+        };
+
+        Scheduler::cancel_scheduled_timer::<ExpiredRequestScheduleStrategy>(strategy, at_ns);
     }
 }
--- core/station/impl/src/jobs/scheduler.rs
+++ core/station/impl/src/jobs/scheduler.rs
@@ -1,210 +1,55 @@
-use std::{
-    cell::RefCell,
-    collections::{HashMap, HashSet},
-    hash::Hash,
-    sync::{atomic::AtomicBool, Arc},
-    time::Duration,
-};
-
-use ic_cdk_timers::TimerId;
 use orbit_essentials::cdk::{api::time, spawn};
 
-use crate::models::RequestId;
+use super::ScheduledJob;
 
-use super::{
-    cancel_expired_requests, execute_created_transfers, execute_scheduled_requests, ScheduledJob,
-};
+pub trait ScheduleStrategy {
+    const TOLERANCE_SEC: u64 = 3;
 
-thread_local! {
-    static CANCEL_EXPIRED_REQUESTS_TIMERS: RefCell<EntityTimerMap<RequestId>> = RefCell::new(EntityTimerMap::default());
-    static EXECUTE_SCHEDULED_REQUESTS_TIMERS: RefCell<HashSet<AbsoluteJobTimeNs>> = RefCell::new(HashSet::new());
-    static EXECUTE_CREATED_TRANSFERS_TIMERS: RefCell<HashSet<AbsoluteJobTimeNs>> = RefCell::new(HashSet::new());
-
-    static CANCEL_EXPIRED_REQUESTS_RUNNING: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
-    static EXECUTE_SCHEDULED_REQUESTS_RUNNING: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
-    static EXECUTE_CREATED_TRANSFERS_RUNNING: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    // If the call results in a new timer, return the timer value.
+    fn add_timer(&self, at_ns: u64) -> Option<u64>;
+    // If the call results in a timer being removed, return the timer value.
+    fn remove_timer(&self, at_ns: u64) -> Option<u64>;
+    // Return whether the job is currently running.
+    fn is_running(&self) -> bool;
+    // Set whether the job is currently running.
+    fn set_running(&self, running: bool);
 }
 
-pub type AbsoluteJobTimeNs = u64;
-pub type RelativeJobTimeNs = u64;
-
-struct ScheduledEntities<T: Eq + PartialEq + Hash> {
-    timer_id: TimerId,
-    entity_ids: HashSet<T>,
-}
-
-#[derive(Default)]
-struct EntityTimerMap<T: Eq + PartialEq + Hash> {
-    time_to_schedules: HashMap<AbsoluteJobTimeNs, ScheduledEntities<T>>,
-    entity_id_to_time: HashMap<T, AbsoluteJobTimeNs>,
-}
-
-impl<T: Eq + PartialEq + Hash> EntityTimerMap<T> {
-    fn insert(&mut self, coarse_time: AbsoluteJobTimeNs, timer_id: TimerId, entity_id: T) {
-        self.time_to_schedules
-            .entry(coarse_time)
-            .or_insert_with(|| ScheduledEntities {
-                entity_ids: HashSet::new(),
-                timer_id,
-            })
-            .entity_ids
-            .insert(entity_id);
-    }
-
-    fn get(&self, time: AbsoluteJobTimeNs) -> Option<&TimerId> {
-        self.time_to_schedules
-            .get(&time)
-            .map(|scheduled_entities| &scheduled_entities.timer_id)
-    }
-
-    fn remove_by_time(&mut self, time: AbsoluteJobTimeNs) {
-        if let Some(scheduled_entities) = self.time_to_schedules.remove(&time) {
-            for entity_id in scheduled_entities.entity_ids {
-                self.entity_id_to_time.remove(&entity_id);
-            }
-        }
-    }
-
-    fn remove_by_entity(&mut self, entity_id: &T) {
-        if let Some(time) = self.entity_id_to_time.remove(entity_id) {
-            if let Some(scheduled_entities) = self.time_to_schedules.get_mut(&time) {
-                scheduled_entities.entity_ids.remove(entity_id);
-
-                if scheduled_entities.entity_ids.is_empty() {
-                    self.time_to_schedules.remove(&time);
-                }
-            }
-        }
-    }
-}
-
-#[derive(Default)]
 pub struct Scheduler;
 
-pub enum JobType {
-    CancelExpiredRequests { request_id: RequestId },
-    ExecuteScheduledRequests,
-    ExecuteCreatedTransfers,
-}
-
 impl Scheduler {
-    pub fn new() -> Self {
-        Self::default()
+    pub fn cancel_scheduled_timer<'a, Strategy: ScheduleStrategy>(
+        strategy: Strategy,
+        timer_at_ns: u64,
+    ) {
+        strategy.remove_timer(timer_at_ns);
     }
 
-    fn coarse_time_to_timeout(time: AbsoluteJobTimeNs, tolerance: Duration) -> AbsoluteJobTimeNs {
-        time - time % tolerance.as_nanos() as AbsoluteJobTimeNs
-    }
-
-    fn absolute_time_to_relative(abs_time: AbsoluteJobTimeNs) -> RelativeJobTimeNs {
+    pub fn schedule<'a, Strategy: ScheduleStrategy + 'static, Job: ScheduledJob>(
+        strategy: Strategy,
+        delay_ns: u64,
+    ) {
         let now = time();
+        let future_time_ns = now + delay_ns;
 
-        if abs_time < now {
-            return 0;
-        }
+        if let Some(timer_at_ns) = strategy.add_timer(future_time_ns) {
+            spawn(async move {
+                if !strategy.is_running() {
+                    strategy.set_running(true);
+                    let job_complete = Job::run().await;
+                    strategy.set_running(false);
+                    strategy.remove_timer(timer_at_ns);
 
-        abs_time - now
-    }
-
-    pub fn cancel_job(&mut self, job_type: JobType) {
-        match job_type {
-            JobType::CancelExpiredRequests { request_id } => {
-                CANCEL_EXPIRED_REQUESTS_TIMERS
-                    .with(|timers| timers.borrow_mut().remove_by_entity(&request_id));
-            }
-            JobType::ExecuteScheduledRequests | JobType::ExecuteCreatedTransfers => {
-                // can't be cancelled
-            }
-        }
-    }
-
-    pub fn schedule_now(&mut self, job_type: JobType) {
-        self.schedule_at(job_type, time());
-    }
-
-    pub fn schedule_at(&mut self, job_type: JobType, abs_time_ns: AbsoluteJobTimeNs) {
-        match job_type {
-            JobType::CancelExpiredRequests { request_id } => {
-                // coarse time
-
-                let coarse_time = Self::coarse_time_to_timeout(
-                    abs_time_ns,
-                    cancel_expired_requests::Job::TIME_TOLERANCE,
-                );
-
-                // check if the timer is already scheduled
-
-                if CANCEL_EXPIRED_REQUESTS_TIMERS
-                    .with(|timers| timers.borrow().get(coarse_time).is_some())
-                {
-                    return;
-                }
-
-                // schedule the timer
-
-                let relative_job_time = Self::absolute_time_to_relative(coarse_time);
-                let timer_id =
-                    ic_cdk_timers::set_timer(Duration::from_nanos(relative_job_time), move || {
-                        spawn(async move {
-                            cancel_expired_requests::Job::run().await;
-
-                            CANCEL_EXPIRED_REQUESTS_TIMERS
-                                .with(|timers| timers.borrow_mut().remove_by_time(coarse_time))
-                        });
-                    });
-
-                CANCEL_EXPIRED_REQUESTS_TIMERS.with(|timers| {
-                    timers
-                        .borrow_mut()
-                        .insert(coarse_time, timer_id, request_id)
-                });
-            }
-            JobType::ExecuteScheduledRequests => {
-                let coarse_time = Self::coarse_time_to_timeout(
-                    abs_time_ns,
-                    execute_scheduled_requests::Job::TIME_TOLERANCE,
-                );
-
-                if EXECUTE_SCHEDULED_REQUESTS_TIMERS
-                    .with(|timers| timers.borrow().get(&coarse_time).is_some())
-                {
-                    return;
-                }
-
-                let relative_job_time = Self::absolute_time_to_relative(coarse_time);
-                ic_cdk_timers::set_timer(Duration::from_nanos(relative_job_time), || {
-                    spawn(async move {
-                        execute_scheduled_requests::Job::run().await;
-                    });
-                });
-
-                EXECUTE_SCHEDULED_REQUESTS_TIMERS
-                    .with(|timers| timers.borrow_mut().insert(coarse_time));
-            }
-            JobType::ExecuteCreatedTransfers => {
-                let coarse_time = Self::coarse_time_to_timeout(
-                    abs_time_ns,
-                    execute_created_transfers::Job::TIME_TOLERANCE,
-                );
-
-                if EXECUTE_CREATED_TRANSFERS_TIMERS
-                    .with(|timers| timers.borrow().get(&coarse_time).is_some())
-                {
-                    return;
-                }
-
-                let relative_job_time = Self::absolute_time_to_relative(coarse_time);
-                ic_cdk_timers::set_timer(Duration::from_nanos(relative_job_time), || {
-                    spawn(async move {
-                        execute_created_transfers::Job::run().await;
-                    });
-                });
-
-                EXECUTE_CREATED_TRANSFERS_TIMERS
-                    .with(|timers| timers.borrow_mut().insert(coarse_time));
-            }
+                    if !job_complete {
+                        Self::schedule::<Strategy, Job>(strategy, 0)
+                    }
+                } else {
+                    Self::schedule::<Strategy, Job>(
+                        strategy,
+                        Strategy::TOLERANCE_SEC * 1_000_000_000,
+                    )
+                };
+            });
         }
     }
-
-    // async fn run_job<Job: ScheduledJob>(&mut self, job: Job) {}
 }
