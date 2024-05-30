39a91298e4faa665c7392abb47c33cf936a298cb
Martin Raszyk <martin.raszyk@dfinity.org>
2024-05-30 12:02:13 +0200

    fix ChangeCanisterOperation.vue

--- apps/wallet/src/components/requests/operations/ChangeCanisterOperation.vue
+++ apps/wallet/src/components/requests/operations/ChangeCanisterOperation.vue
@@ -46,8 +46,6 @@ const target = computed(() => {
     return i18n.t('terms.station');
   }
 
-  if (variantIs(props.operation.target, 'UpgradeUpgrader')) {
-    return i18n.t('terms.upgrader');
-  }
+  return i18n.t('terms.upgrader');
 });
 </script>
