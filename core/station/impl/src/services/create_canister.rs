50cb80fd10ed86a81798e6d33cad71840e36a0d4
Martin Raszyk <martin.raszyk@dfinity.org>
2024-06-03 11:37:59 +0200

    refactor

--- core/station/impl/src/services/create_canister.rs
+++ core/station/impl/src/services/create_canister.rs
@@ -16,17 +16,17 @@ const CREATE_CANISTER_CYCLES: u128 = 100_000_000_000; // the default fee of 100 
 pub struct CreateCanisterService {}
 
 impl CreateCanisterService {
-    /// Execute an install or upgrade of a canister.
     pub async fn create_canister(&self) -> ServiceResult<Principal, ChangeCanisterError> {
         let create_canister_arg = CreateCanisterArgument { settings: None };
-        Ok(
-            mgmt::create_canister(create_canister_arg, CREATE_CANISTER_CYCLES)
-                .await
-                .map_err(|(_, err)| ChangeCanisterError::Failed {
-                    reason: err.to_string(),
-                })?
-                .0
-                .canister_id,
-        )
+
+        let canister_id = mgmt::create_canister(create_canister_arg, CREATE_CANISTER_CYCLES)
+            .await
+            .map_err(|(_, err)| ChangeCanisterError::Failed {
+                reason: err.to_string(),
+            })?
+            .0
+            .canister_id;
+
+        Ok(canister_id)
     }
 }
