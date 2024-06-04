786e736cbe4af59667006941214389e36e9e6781
Kepler Vital <dev@keplervital.com>
2024-06-04 17:19:03 +0200

    add mapper tests

--- core/control-panel/impl/src/mappers/artifact.rs
+++ core/control-panel/impl/src/mappers/artifact.rs
@@ -13,3 +13,23 @@ impl From<Artifact> for control_panel_api::ArtifactDTO {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_model_conversion() {
+        let model = Artifact::new(b"hello world".to_vec());
+        let dto = control_panel_api::ArtifactDTO::from(model.clone());
+
+        assert_eq!(dto.id, Uuid::from_bytes(*model.id()).to_string());
+        assert_eq!(dto.size, model.artifact().len() as u64);
+        assert_eq!(
+            dto.hash,
+            "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9".to_string()
+        );
+        assert_eq!(dto.artifact, model.artifact());
+        assert_eq!(dto.created_at, timestamp_to_rfc3339(&model.created_at()));
+    }
+}
