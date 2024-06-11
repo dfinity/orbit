ac6fd6b400af153a16264676383731d2d885121e
Max Murphy <max@winning.black>
2024-06-11 13:50:20 +0200

    Add clap library

--- tools/dfx-orbit/src/main.rs
+++ tools/dfx-orbit/src/main.rs
@@ -1,3 +1,7 @@
+//! # `dfx orbit` tool for managing digital assets.
+//!
+//! Note: This will initially be a standalone executable, but will be converted into a dfx extension once the dfx subcommand extension framework is well defined.
+
 fn main() {
     println!("Hello, world!");
 }
