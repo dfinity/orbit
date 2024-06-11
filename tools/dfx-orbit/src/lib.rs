0a548f30c3a5a47a93adcf945e988e12663769e1
Max Murphy <max@winning.black>
2024-06-11 14:28:03 +0200

    Define wallet subcommands

--- tools/dfx-orbit/src/lib.rs
+++ tools/dfx-orbit/src/lib.rs
@@ -1,10 +1,16 @@
 //! Library for interacting with Orbit on the Internet Computer.
 use clap::Parser;
 
+pub mod wallet;
+use wallet::OrbitWalletArgs;
+
 /// Manages Orbit on the Internet Computer.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct DfxOrbitArgs {
+    /// Manage Orbit wallets.
+    #[command(subcommand)]
+    field: OrbitWalletArgs,
 }
 
 pub fn main(args: DfxOrbitArgs) {
--- tools/dfx-orbit/src/main.rs
+++ tools/dfx-orbit/src/main.rs
@@ -1,8 +1,8 @@
 //! # `dfx orbit` tool for managing digital assets.
 //!
 //! Note: This will initially be a standalone executable, but will be converted into a dfx extension once the dfx subcommand extension framework is well defined.
-use dfx_orbit::{self as lib, DfxOrbitArgs};
 use clap::Parser;
+use dfx_orbit::{self as lib, DfxOrbitArgs};
 
 fn main() {
     let args = DfxOrbitArgs::parse();
