16a191594236f39a7a472377df5ea1a1c48d7d39
Max Murphy <max@winning.black>
2024-06-12 11:01:04 +0200

    Move code to a submodule

--- tools/dfx-orbit/src/cli.rs
+++ tools/dfx-orbit/src/cli.rs
@@ -1,7 +1,7 @@
 //! Implementation of the `dfx-orbit` commands.
+pub mod dfx_extension_cli;
 
-use crate::args::{dfx_extension_api, DfxOrbitArgs, DfxOrbitSubcommands};
-use std::io::Read;
+use crate::args::{DfxOrbitArgs, DfxOrbitSubcommands};
 
 /// A command line tool for interacting with Orbit on the Internet Computer.
 pub fn main(args: DfxOrbitArgs) {
@@ -13,27 +13,7 @@ pub fn main(args: DfxOrbitArgs) {
         }
         DfxOrbitSubcommands::DfxExtension(dfx_extension_args) => {
             println!("Hello dfx extension args: {dfx_extension_args:?}");
-            match dfx_extension_args {
-                dfx_extension_api::Args::Config(config_args) => match config_args {
-                    dfx_extension_api::config::Args::Dir => {
-                        let extension_agent =
-                            crate::dfx_extension_api::DfxExtensionAgent::new("orbit");
-                        let ans = extension_agent.extension_config_dir();
-                        println!("{ans:?}");
-                    }
-                    dfx_extension_api::config::Args::File => {
-                        let extension_agent =
-                            crate::dfx_extension_api::DfxExtensionAgent::new("orbit");
-                        let mut file = extension_agent
-                            .extension_config_file()
-                            .expect("Could not open file");
-                        let mut ans = String::new();
-                        file.read_to_string(&mut ans).expect("Could not read file");
-                        // let config: crate::local_config::CommonConfig = serde_json::from_reader(&mut file).unwrap();
-                        println!("{ans:?}");
-                    }
-                },
-            }
+            dfx_extension_cli::main(dfx_extension_args);
         }
     }
 }
