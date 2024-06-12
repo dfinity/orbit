ac118cad0927a30b2757852a0e58f359a8bfc322
Max Murphy <max@winning.black>
2024-06-12 12:58:57 +0200

    Remove println

--- tools/dfx-orbit/src/cli.rs
+++ tools/dfx-orbit/src/cli.rs
@@ -6,8 +6,6 @@ use crate::args::{DfxOrbitArgs, DfxOrbitSubcommands};
 
 /// A command line tool for interacting with Orbit on the Internet Computer.
 pub fn main(args: DfxOrbitArgs) {
-    println!("Hello args: {args:?}");
-
     match args.command {
         DfxOrbitSubcommands::Station(station_args) => {
             station::main(station_args);
