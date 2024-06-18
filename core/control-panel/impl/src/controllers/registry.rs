b50c427bfca1432519919ffad760bf963f16ea02
Kepler Vital <dev@keplervital.com>
2024-06-18 10:09:32 +0200

    add tests for auth middleware

--- core/control-panel/impl/src/controllers/registry.rs
+++ core/control-panel/impl/src/controllers/registry.rs
@@ -143,6 +143,7 @@ impl RegistryController {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::core::ic_cdk::{api::id as self_canister_id, set_caller};
     use crate::errors::RegistryError;
     use orbit_essentials::api::ApiError;
     use uuid::Uuid;
@@ -160,5 +161,197 @@ mod tests {
             result.unwrap_err(),
             ApiError::from(RegistryError::NotFound { id: entry_id })
         );
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn test_add_registry_entry_not_allowed() {
+        let _ = CONTROLLER
+            .add_registry_entry(control_panel_api::AddRegistryEntryInput {
+                entry: control_panel_api::RegistryEntryInput {
+                    name: "test".to_string(),
+                    description: "This is a demo description for the package.".to_string(),
+                    categories: vec![],
+                    tags: vec![],
+                    metadata: vec![],
+                    value: control_panel_api::RegistryEntryValueInput::WasmModule(
+                        control_panel_api::WasmModuleRegistryEntryValueInput {
+                            version: "0.1.0".to_string(),
+                            dependencies: vec![],
+                            wasm_module: vec![],
+                        },
+                    ),
+                },
+            })
+            .await;
+    }
+
+    #[tokio::test]
+    async fn test_add_registry_entry_is_allowed() {
+        set_caller(self_canister_id());
+
+        let result = CONTROLLER
+            .add_registry_entry(control_panel_api::AddRegistryEntryInput {
+                entry: control_panel_api::RegistryEntryInput {
+                    name: "test".to_string(),
+                    description: "This is a demo description for the package.".to_string(),
+                    categories: vec![],
+                    tags: vec![],
+                    metadata: vec![],
+                    value: control_panel_api::RegistryEntryValueInput::WasmModule(
+                        control_panel_api::WasmModuleRegistryEntryValueInput {
+                            version: "0.1.0".to_string(),
+                            dependencies: vec![],
+                            wasm_module: vec![1, 2, 3],
+                        },
+                    ),
+                },
+            })
+            .await;
+
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_edit_registry_entry_not_found() {
+        set_caller(self_canister_id());
+
+        let entry_id = Uuid::new_v4().to_string();
+        let result = CONTROLLER
+            .edit_registry_entry(EditRegistryEntryInput {
+                id: entry_id.clone(),
+                entry: control_panel_api::RegistryEntryUpdateInput {
+                    description: None,
+                    categories: Some(vec!["test".to_string()]),
+                    tags: None,
+                    metadata: None,
+                    value: None,
+                },
+            })
+            .await;
+
+        assert_eq!(
+            result.unwrap_err(),
+            ApiError::from(RegistryError::NotFound { id: entry_id })
+        );
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn test_edit_registry_entry_not_allowed() {
+        let _ = CONTROLLER
+            .edit_registry_entry(control_panel_api::EditRegistryEntryInput {
+                id: Uuid::new_v4().to_string(),
+                entry: control_panel_api::RegistryEntryUpdateInput {
+                    description: None,
+                    categories: Some(vec!["test".to_string()]),
+                    tags: None,
+                    metadata: None,
+                    value: None,
+                },
+            })
+            .await;
+    }
+
+    #[tokio::test]
+    async fn test_edit_registry_entry_allowed() {
+        set_caller(self_canister_id());
+
+        let response = CONTROLLER
+            .add_registry_entry(control_panel_api::AddRegistryEntryInput {
+                entry: control_panel_api::RegistryEntryInput {
+                    name: "test".to_string(),
+                    description: "This is a demo description for the package.".to_string(),
+                    categories: vec![],
+                    tags: vec![],
+                    metadata: vec![],
+                    value: control_panel_api::RegistryEntryValueInput::WasmModule(
+                        control_panel_api::WasmModuleRegistryEntryValueInput {
+                            version: "0.1.0".to_string(),
+                            dependencies: vec![],
+                            wasm_module: vec![1, 2, 3],
+                        },
+                    ),
+                },
+            })
+            .await
+            .unwrap();
+
+        assert!(response.entry.categories.is_empty());
+
+        let edited = CONTROLLER
+            .edit_registry_entry(control_panel_api::EditRegistryEntryInput {
+                id: response.entry.id,
+                entry: control_panel_api::RegistryEntryUpdateInput {
+                    description: None,
+                    categories: Some(vec!["test".to_string()]),
+                    tags: None,
+                    metadata: None,
+                    value: None,
+                },
+            })
+            .await
+            .unwrap();
+
+        assert_eq!(edited.entry.categories, vec!["test".to_string()]);
+    }
+
+    #[tokio::test]
+    async fn test_delete_registry_entry_not_found() {
+        set_caller(self_canister_id());
+
+        let entry_id = Uuid::new_v4().to_string();
+        let result = CONTROLLER
+            .delete_registry_entry(DeleteRegistryEntryInput {
+                id: entry_id.clone(),
+            })
+            .await;
+
+        assert_eq!(
+            result.unwrap_err(),
+            ApiError::from(RegistryError::NotFound { id: entry_id })
+        );
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn test_delete_registry_entry_not_allowed() {
+        let _ = CONTROLLER
+            .delete_registry_entry(control_panel_api::DeleteRegistryEntryInput {
+                id: Uuid::new_v4().to_string(),
+            })
+            .await;
+    }
+
+    #[tokio::test]
+    async fn test_delete_registry_entry_allowed() {
+        set_caller(self_canister_id());
+
+        let response = CONTROLLER
+            .add_registry_entry(control_panel_api::AddRegistryEntryInput {
+                entry: control_panel_api::RegistryEntryInput {
+                    name: "test".to_string(),
+                    description: "This is a demo description for the package.".to_string(),
+                    categories: vec![],
+                    tags: vec![],
+                    metadata: vec![],
+                    value: control_panel_api::RegistryEntryValueInput::WasmModule(
+                        control_panel_api::WasmModuleRegistryEntryValueInput {
+                            version: "0.1.0".to_string(),
+                            dependencies: vec![],
+                            wasm_module: vec![1, 2, 3],
+                        },
+                    ),
+                },
+            })
+            .await
+            .unwrap();
+
+        let _ = CONTROLLER
+            .delete_registry_entry(control_panel_api::DeleteRegistryEntryInput {
+                id: response.entry.id,
+            })
+            .await
+            .unwrap();
     }
 }
