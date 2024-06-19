360890ddf53374cd93f93e961892f545a7cc5293
Max Murphy <max@winning.black>
2024-06-19 16:34:26 +0200

    Rm unused

--- tools/dfx-orbit/src/args/request/permission/canister.rs
+++ tools/dfx-orbit/src/args/request/permission/canister.rs
@@ -1,5 +1,4 @@
 //! Makes `EditPermission` requests regarding `ExternalCanister` to Orbit.
-use candid::Principal;
 use clap::{Parser, Subcommand};
 
 use crate::{args::request::CreateRequestArgs, orbit_station_agent::StationAgent};
@@ -66,42 +65,5 @@ impl CreateRequestArgs for ChangeCanister {
             summary: None,
             execution_plan: None,
         })
-    }
-}
-
-impl TryFrom<Args> for orbit_station_api::RequestOperationInput {
-    type Error = anyhow::Error;
-    fn try_from(args: Args) -> anyhow::Result<Self> {
-        match args {
-            Args::Change(change_args) => Ok(
-                orbit_station_api::RequestOperationInput::EditPermission(change_args.try_into()?),
-            ),
-        }
-    }
-}
-
-impl TryFrom<ChangeCanister> for orbit_station_api::EditPermissionOperationInput {
-    type Error = anyhow::Error;
-
-    fn try_from(args: ChangeCanister) -> anyhow::Result<Self> {
-        if let Some(canister_name_or_id) = args.canister {
-            // Grant permission for just this one canister.
-            let canister_id = Principal::from_text(canister_name_or_id)?;
-            let resource = orbit_station_api::ResourceDTO::ExternalCanister(
-                orbit_station_api::ExternalCanisterResourceActionDTO::Change(
-                    orbit_station_api::ChangeExternalCanisterResourceTargetDTO::Canister(
-                        canister_id,
-                    ),
-                ),
-            );
-            Ok(orbit_station_api::EditPermissionOperationInput {
-                resource,
-                auth_scope: None,
-                users: None,
-                user_groups: None,
-            })
-        } else {
-            unimplemented!("Need to implement granting access to all canisters.")
-        }
     }
 }
