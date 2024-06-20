a2d1f006eee4c202e19673b2f5f9477d748d9953
Max Murphy <max@winning.black>
2024-06-20 19:39:30 +0200

    Look up canister id

--- tools/dfx-orbit/src/cli/canister/upload_http_assets.rs
+++ tools/dfx-orbit/src/cli/canister/upload_http_assets.rs
@@ -4,9 +4,10 @@ use crate::args::canister::UploadHttpAssets as Args;
 /// The main entry point for the `dfx orbit` CLI.
 pub async fn exec(args: Args) -> anyhow::Result<()> {
     let Args {
-        canister: _canister,
+        canister,
         path: _path,
     } = args;
-    let _station_agent = crate::orbit_station_agent::StationAgent::new()?;
+    let station_agent = crate::orbit_station_agent::StationAgent::new()?;
+    let _canister_id = station_agent.canister_id(&canister)?;
     todo!()
 }
