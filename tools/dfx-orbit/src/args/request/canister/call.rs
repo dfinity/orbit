9b0cbe81072dbd1b7155b24eb1ae1a9f2aabbca8
Max Murphy <max@winning.black>
2024-07-02 20:53:02 +0200

    Basic cli arg conversion

--- tools/dfx-orbit/src/args/request/canister/call.rs
+++ tools/dfx-orbit/src/args/request/canister/call.rs
@@ -1,6 +1,7 @@
 //! CLI arguments for `dfx-orbit canister call`.
 
 use clap::Parser;
+use orbit_station_api::{CallExternalCanisterOperationInput, CanisterMethodDTO};
 
 use crate::{args::request::CreateRequestArgs, orbit_station_agent::StationAgent};
 
@@ -10,51 +11,44 @@ pub struct Args {
     /// The canister name or ID.
     canister: String,
     /// The name of the method to call.
-    method: String,
-    /// The argument to pass to the canister.
-    argument: Option<String>,
+    method_name: String,
+    // TODO:
+    // /// The argument to pass to the canister.
+    // argument: Option<String>,
+    // TODO:
+    // /// The format of the argument.
+    // #[clap(short, long)]
+    // r#type: Option<CandidFormat>,
+    // TODO: Read argument from a file
 }
 
 impl CreateRequestArgs for Args {
     /// Converts the CLI arg type into the equivalent Orbit API type.
     fn into_create_request_input(
         self,
-        _station_agent: &StationAgent,
+        station_agent: &StationAgent,
     ) -> anyhow::Result<orbit_station_api::CreateRequestInput> {
-        /*
         let Args {
-            canister,method,argument
+            canister,
+            method_name,
         } = self;
         let canister_id = station_agent.canister_id(&canister)?;
-        let operation = {
-            let module = std::fs::read(wasm)
-                .expect("Could not read Wasm file")
-                .to_vec();
-            let arg = if let Some(file) = arg_file {
-                Some(
-                    std::fs::read(file)
-                        .expect("Could not read argument file")
-                        .to_vec(),
-                )
-            } else {
-                arg.map(|arg| arg.as_bytes().to_vec())
-            };
-            let mode = mode.into();
-            orbit_station_api::ChangeExternalCanisterOperationInput {
-                canister_id,
-                mode,
-                module,
-                arg,
-            }
-        };
-        let operation = orbit_station_api::RequestOperationInput::ChangeExternalCanister(operation);
+        let operation = orbit_station_api::RequestOperationInput::CallExternalCanister(
+            CallExternalCanisterOperationInput {
+                validation_method: None,
+                execution_method: CanisterMethodDTO {
+                    canister_id,
+                    method_name,
+                },
+                arg: None,
+                execution_method_cycles: None, // TODO: Add this to the CLI
+            },
+        );
         Ok(orbit_station_api::CreateRequestInput {
             operation,
             title: None,
             summary: None,
             execution_plan: None,
         })
-        */
-        unimplemented!("Argument conversion not implemented yet")
     }
 }
