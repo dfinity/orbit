6e8791c4e57fa587a25a7108dbfe0b616586b81f
Leon Tan <leon.tan@dfinity.org>
2024-07-16 13:15:56 +0000

    Reintroduce arg parsing

--- tools/dfx-orbit/src/args/review/id.rs
+++ tools/dfx-orbit/src/args/review/id.rs
@@ -1,9 +1,18 @@
 //! CLI arguments for `dfx-orbit review next`.
 use clap::Parser;
+use orbit_station_api::GetRequestInput;
 
 /// Reviews the next request.
 #[derive(Debug, Parser)]
 pub struct ReviewIdArgs {
     /// The ID of the request to review.
     pub(crate) request_id: String,
+}
+
+impl From<ReviewIdArgs> for GetRequestInput {
+    fn from(args: ReviewIdArgs) -> Self {
+        GetRequestInput {
+            request_id: args.request_id,
+        }
+    }
 }
