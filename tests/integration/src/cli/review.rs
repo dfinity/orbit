a06d6b480b5597f9cc3f755aa6cce2fcfd0cec5e
Leon Tan <leon.tan@dfinity.org>
2024-07-22 16:15:09 +0000

    WIP implement review functionality

--- tests/integration/src/cli/review.rs
+++ tests/integration/src/cli/review.rs
@@ -1,17 +1,23 @@
 use candid::Principal;
+use pocket_ic::PocketIc;
+use station_api::{
+    AuthScopeDTO, CallExternalCanisterOperationInput, CanisterMethodDTO,
+    EditPermissionOperationInput, ListRequestsInput, RequestOperationInput,
+    RequestResourceActionDTO, ResourceDTO,
+};
 
 use crate::{
     cli::{
         canister_call::{permit_call_operation, set_four_eyes_on_call},
-        setup_counter_canister, TEST_PRINCIPAL,
+        dfx_orbit_test, setup_agent, setup_counter_canister, TEST_PRINCIPAL,
     },
-    setup::setup_new_env,
-    utils::{add_user, add_user_with_name, user_test_id},
-    TestEnv,
+    setup::{setup_new_env, WALLET_ADMIN_USER},
+    utils::{add_user, add_user_with_name, execute_request, submit_request, user_test_id},
+    CanisterIds, TestEnv,
 };
 
 #[test]
-fn review_canister_upgrate() {
+fn review() {
     let TestEnv {
         mut env,
         canister_ids,
@@ -32,8 +38,68 @@ fn review_canister_upgrate() {
     let other_user = user_test_id(1);
     add_user(&env, other_user, vec![], canister_ids.station);
 
+    permit_list_reads(&env, &canister_ids);
     permit_call_operation(&env, &canister_ids);
     set_four_eyes_on_call(&env, &canister_ids);
 
+    // The other user submits a canister call request
+    let request_counter_canister_inc =
+        RequestOperationInput::CallExternalCanister(CallExternalCanisterOperationInput {
+            validation_method: None,
+            execution_method: CanisterMethodDTO {
+                canister_id,
+                method_name: String::from("set"),
+            },
+            arg: Some(42_u32.to_le_bytes().to_vec()),
+            execution_method_cycles: None,
+        });
+
+    let submitted_request = submit_request(
+        &env,
+        other_user,
+        canister_ids.station,
+        request_counter_canister_inc,
+    );
+
+    dfx_orbit_test(&mut env, async {
+        // Setup the station agent
+        let mut station_agent = setup_agent(canister_ids.station).await;
+
+        let list_request_response = station_agent
+            .review_list(ListRequestsInput {
+                requester_ids: None,
+                approver_ids: None,
+                statuses: None,
+                operation_types: None,
+                expiration_from_dt: None,
+                expiration_to_dt: None,
+                created_from_dt: None,
+                created_to_dt: None,
+                paginate: None,
+                sort_by: None,
+                only_approvable: true,
+                with_evaluation_results: false,
+            })
+            .await
+            .unwrap();
+
+        dbg!(&list_request_response);
+
+        // Check that there is only one entry in the response and that its the one that was submitted
+        assert_eq!(list_request_response.requests.len(), 1);
+        assert_eq!(list_request_response.requests[0].id, submitted_request.id);
+    });
+
     todo!()
+}
+
+/// Allow anyone to read request list
+pub(crate) fn permit_list_reads(env: &PocketIc, canister_ids: &CanisterIds) {
+    let add_permission = RequestOperationInput::EditPermission(EditPermissionOperationInput {
+        resource: ResourceDTO::Request(RequestResourceActionDTO::List),
+        auth_scope: Some(AuthScopeDTO::Authenticated),
+        user_groups: None,
+        users: None,
+    });
+    execute_request(env, WALLET_ADMIN_USER, canister_ids.station, add_permission).unwrap();
 }
