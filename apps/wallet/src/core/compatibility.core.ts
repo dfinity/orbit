870b2cac38a6f6b323ef280d1cd2660281b6e800
Kepler Vital <dev@keplervital.com>
2024-07-25 14:04:37 +0200

    feat(wallet): remove unsafe-eval csp by bumping the agent

--- apps/wallet/src/core/compatibility.core.ts
+++ apps/wallet/src/core/compatibility.core.ts
@@ -1,4 +1,4 @@
-import { Certificate, HttpAgent } from '@dfinity/agent';
+import { Certificate, HttpAgent, LookupStatus } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
 import { appInitConfig } from '~/configs/init.config';
 import { icAgent } from '~/core/ic-agent.core';
@@ -33,12 +33,16 @@ async function fetchStationApiVersion(agent: HttpAgent, stationId: Principal): P
 
   const version = certificate.lookup(versionPath);
 
-  if (!version) {
+  if (version.status !== LookupStatus.Found) {
     throw new Error('Version not found');
   }
 
+  if (!(version.value instanceof ArrayBuffer)) {
+    throw new Error('Version value is not an ArrayBuffer');
+  }
+
   const decoder = new TextDecoder();
-  const decodedVersion = decoder.decode(version);
+  const decodedVersion = decoder.decode(version.value);
 
   if (!isSemanticVersion(decodedVersion)) {
     throw new Error(
