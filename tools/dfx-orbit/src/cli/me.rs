91015894b87f889610eafba3262ce143a1a9bdcb
Leon Tan <leon.tan@dfinity.org>
2024-07-26 13:16:23 +0000

    Use newer API with me

--- tools/dfx-orbit/src/cli/me.rs
+++ tools/dfx-orbit/src/cli/me.rs
@@ -1,27 +1,10 @@
 //! Implementation of the `dfx-orbit me` command.
 
 use crate::{error::StationAgentResult, StationAgent};
-use anyhow::Context;
-use candid::encode_args;
-use orbit_station_api::{ApiErrorDTO, MeResponse};
+use orbit_station_api::MeResponse;
 
 impl StationAgent {
     pub async fn me(&mut self) -> StationAgentResult<MeResponse> {
-        let ans = self
-            .update_orbit("me")
-            .await?
-            .with_arg(empty_args())
-            .call_and_wait()
-            .await
-            .with_context(|| "Failed to make API call")?;
-        let ans: Result<MeResponse, ApiErrorDTO> =
-            candid::decode_one(&ans).with_context(|| "Failed to decode response")?;
-
-        Ok(ans?)
+        self.update_orbit_typed("me", ()).await
     }
-}
-
-/// Encodes an empty tuple as Candid.  This is used for methods with no arguments.
-pub fn empty_args() -> Vec<u8> {
-    encode_args(()).expect("Failed to candid encode empty tuple")
 }
