808e4be6c2274d6905b84025bba5a18114d1750f
Kepler Vital <dev@keplervital.com>
2024-07-29 11:15:56 +0200

    feat(wallet): add advanced and basic mode to update modal

---
+++ apps/wallet/src/components/layouts/AppToolbar.spec.ts
@@ -0,0 +1,115 @@
+import { describe, expect, it } from 'vitest';
+import { mount } from '~/test.utils';
+import AppToolbar from './AppToolbar.vue';
+
+describe('AppToolbar', () => {
+  it('shows update when new version is available', () => {
+    const wrapper = mount(
+      AppToolbar,
+      {},
+      {
+        initialPiniaState: {
+          session: { isAuthenticated: true },
+          station: {
+            privileges: [{ ChangeCanister: null }],
+            versionManagement: {
+              nextStationVersion: '1.2.3',
+              nextUpgraderVersion: '1.2.3',
+              updateRequested: undefined,
+            },
+          },
+        },
+      },
+    );
+
+    expect(wrapper.find('[data-test-id="submit-upgrade-btn"]').exists()).toBe(true);
+  });
+
+  it('hides update if a request is already in progress', () => {
+    const wrapper = mount(
+      AppToolbar,
+      {},
+      {
+        initialPiniaState: {
+          session: { isAuthenticated: true },
+          station: {
+            privileges: [{ ChangeCanister: null }],
+            versionManagement: {
+              nextStationVersion: '1.2.3',
+              nextUpgraderVersion: '1.2.3',
+              updateRequested: '01d71432-d654-4008-8bbe-1d16ccb949d6',
+            },
+          },
+        },
+      },
+    );
+
+    expect(wrapper.find('[data-test-id="submit-upgrade-btn"]').exists()).toBe(false);
+  });
+
+  it('hides update if no new version is available', () => {
+    const wrapper = mount(
+      AppToolbar,
+      {},
+      {
+        initialPiniaState: {
+          session: { isAuthenticated: true },
+          station: {
+            privileges: [{ ChangeCanister: null }],
+            versionManagement: {
+              nextStationVersion: undefined,
+              nextUpgraderVersion: undefined,
+              updateRequested: undefined,
+            },
+          },
+        },
+      },
+    );
+
+    expect(wrapper.find('[data-test-id="submit-upgrade-btn"]').exists()).toBe(false);
+  });
+
+  it('hides update if user has no privileges', () => {
+    const wrapper = mount(
+      AppToolbar,
+      {},
+      {
+        initialPiniaState: {
+          session: { isAuthenticated: true },
+          station: {
+            privileges: [],
+            versionManagement: {
+              nextStationVersion: '1.2.3',
+              nextUpgraderVersion: '1.2.3',
+              updateRequested: undefined,
+            },
+          },
+        },
+      },
+    );
+
+    expect(wrapper.find('[data-test-id="submit-upgrade-btn"]').exists()).toBe(false);
+  });
+
+  it('hides update if user is not authenticated', () => {
+    const wrapper = mount(
+      AppToolbar,
+      {},
+      {
+        initialPiniaState: {
+          session: { isAuthenticated: false },
+          station: {
+            privileges: [{ ChangeCanister: null }],
+            versionManagement: {
+              nextStationVersion: '1.2.3',
+              nextUpgraderVersion: '1.2.3',
+              updateRequested: undefined,
+            },
+          },
+        },
+      },
+    );
+
+    expect(wrapper.find('[data-test-id="submit-upgrade-btn"]').exists()).toBe(false);
+  });
+});
