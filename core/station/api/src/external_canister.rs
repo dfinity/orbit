1a13ca0435661a4d34c3b50b7f04a3e5d50a9608
Kepler Vital <dev@keplervital.com>
2024-07-30 17:04:45 +0200

    feat(station): address book of external canisters

--- core/station/api/src/external_canister.rs
+++ core/station/api/src/external_canister.rs
@@ -1,8 +1,22 @@
-use crate::Sha256HashDTO;
+use crate::{
+    AllowDTO, PaginationInput, RequestPolicyRuleDTO, Sha256HashDTO, TimestampRfc3339, UuidDTO,
+    ValidationMethodResourceTargetDTO,
+};
 use candid::{CandidType, Deserialize, Principal};
 
+pub type ExternalCanisterPermissionsInput = ExternalCanisterPermissionsDTO;
+pub type ExternalCanisterCallRequestPolicyRuleInput = ExternalCanisterCallRequestPolicyRule;
+
 #[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
-pub struct CreateExternalCanisterOperationInput {}
+pub struct CreateExternalCanisterOperationInput {
+    existing_canister_id: Option<Principal>,
+    initial_cycles: Option<u64>,
+    name: String,
+    description: Option<String>,
+    labels: Option<Vec<String>>,
+    permissions: ExternalCanisterPermissionsInput,
+    request_policies: Vec<ExternalCanisterCallRequestPolicyRuleInput>,
+}
 
 #[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
 pub struct CreateExternalCanisterOperationDTO {
@@ -32,4 +46,96 @@ pub struct CallExternalCanisterOperationDTO {
     pub arg_rendering: Option<String>,
     pub execution_method_cycles: Option<u64>,
     pub execution_method_reply: Option<Vec<u8>>,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct ExternalCanisterCallPermissionDTO {
+    pub allow: AllowDTO,
+    pub validation_method: ValidationMethodResourceTargetDTO,
+    pub execution_method: String,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct ExternalCanisterCallRequestPolicyRule {
+    pub rule: RequestPolicyRuleDTO,
+    pub validation_method: ValidationMethodResourceTargetDTO,
+    pub execution_method: String,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct ExternalCanisterPermissionsDTO {
+    pub read: AllowDTO,
+    pub change: AllowDTO,
+    pub calls: Vec<ExternalCanisterCallPermissionDTO>,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct ExternalCanisterDTO {
+    pub id: UuidDTO,
+    pub canister_id: Principal,
+    pub name: String,
+    pub description: Option<String>,
+    pub labels: Vec<String>,
+    pub permissions: ExternalCanisterPermissionsDTO,
+    pub request_policies: Vec<ExternalCanisterCallRequestPolicyRule>,
+    pub created_at: TimestampRfc3339,
+    pub modified_at: Option<TimestampRfc3339>,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub enum GetExternalCanisterInput {
+    Id(UuidDTO),
+    CanisterId(Principal),
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct ExternalCanisterCallerMethodPrivilegesDTO {
+    pub validation_method: ValidationMethodResourceTargetDTO,
+    pub execution_method: String,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct ExternalCanisterCallerPrivilegesDTO {
+    pub id: UuidDTO,
+    pub can_change: bool,
+    pub can_call: Vec<ExternalCanisterCallerMethodPrivilegesDTO>,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct GetExternalCanisterResponse {
+    pub canister: ExternalCanisterDTO,
+    pub privileges: ExternalCanisterCallerPrivilegesDTO,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct ListExternalCanistersInput {
+    pub names: Option<Vec<String>>,
+    pub canister_ids: Option<Vec<Principal>>,
+    pub labels: Option<Vec<String>>,
+    pub paginate: Option<PaginationInput>,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct ListExternalCanistersResponse {
+    pub canisters: Vec<ExternalCanisterDTO>,
+    pub next_offset: Option<u64>,
+    pub total: u64,
+    pub privileges: Vec<ExternalCanisterCallerPrivilegesDTO>,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct GetExternalCanisterFiltersInputWithName {
+    prefix: Option<String>,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct GetExternalCanisterFiltersInput {
+    with_name: Option<GetExternalCanisterFiltersInputWithName>,
+    with_labels: Option<bool>,
+}
+
+#[derive(CandidType, serde::Serialize, Deserialize, Debug, Clone)]
+pub struct GetExternalCanisterFiltersResponse {
+    pub names: Vec<String>,
+    pub labels: Vec<String>,
 }
