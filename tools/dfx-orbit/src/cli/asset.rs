59893f1478aad139f8c01536534b535eee29bc3e
Leon Tan <leon.tan@dfinity.org>
2024-08-16 14:48:19 +0000

    Refactor asset execution

--- tools/dfx-orbit/src/cli/asset.rs
+++ tools/dfx-orbit/src/cli/asset.rs
@@ -5,7 +5,10 @@ mod upload;
 mod util;
 
 use crate::{
-    args::asset::{AssetArgs, AssetArgsAction},
+    args::asset::{
+        AssetArgs, AssetArgsAction, AssetCheckArgs, AssetComputeEvidenceArgs,
+        AssetReqeustPreparePermissionArgs, AssetUploadArgs,
+    },
     DfxOrbit,
 };
 use candid::Principal;
@@ -22,81 +25,11 @@ impl DfxOrbit {
     pub async fn exec_asset(&mut self, args: AssetArgs) -> anyhow::Result<()> {
         match args.action {
             AssetArgsAction::RequestPreparePermission(args) => {
-                let canister_id = self.canister_id(&args.canister)?;
-                let request = self
-                    .request_prepare_permission(canister_id, args.title, args.summary)
-                    .await?;
-                self.print_create_request_info(&request);
-
-                Ok(())
+                self.asset_request_prepare_permission(args).await
             }
-            AssetArgsAction::Upload(args) => {
-                let pathbufs = self.as_path_bufs(&args.canister, &args.files)?;
-                let paths = Self::as_paths(&pathbufs);
-
-                let canister_name = args.canister;
-                let canister_id = self.canister_id(&canister_name)?;
-                let (batch_id, evidence) = self
-                    .upload(canister_id, &paths, args.ignore_evidence)
-                    .await?;
-
-                let result = self
-                    .request_commit_batch(
-                        canister_id,
-                        batch_id.clone(),
-                        evidence,
-                        args.title,
-                        args.summary,
-                    )
-                    .await?;
-                let request_id = result.request.id;
-
-                let files = args.files.join(" ");
-                println!("Created request to commit batches. To verify the batch against local files, run:");
-                println!("dfx-orbit asset check {canister_name} {request_id} {batch_id} {files}");
-
-                Ok(())
-            }
-            AssetArgsAction::ComputeEvidence(args) => {
-                let pathbufs = self.as_path_bufs(&args.canister, &args.files)?;
-                let paths = Self::as_paths(&pathbufs);
-
-                let canister_id = self.canister_id(&args.canister)?;
-                let asset_agent = self.asset_agent(canister_id)?;
-
-                let evidence = asset_agent.compute_evidence(&paths).await?;
-                println!("{evidence}");
-                Ok(())
-            }
-            AssetArgsAction::Check(args) => {
-                let pathbufs = self.as_path_bufs(&args.canister, &args.files)?;
-                let paths = Self::as_paths(&pathbufs);
-
-                let canister_id = self.canister_id(&args.canister)?;
-                let asset_agent = self.asset_agent(canister_id)?;
-
-                let evidence = asset_agent.compute_evidence(&paths).await?;
-                self.check_evidence(
-                    canister_id,
-                    args.request_id.clone(),
-                    args.batch_id,
-                    evidence,
-                )
-                .await?;
-
-                println!("Local evidence matches expected arguments");
-
-                if args.then_approve {
-                    dfx_core::cli::ask_for_consent("Do you want to approve the request?")?;
-                    let args = SubmitRequestApprovalInput {
-                        decision: RequestApprovalStatusDTO::Approved,
-                        request_id: args.request_id,
-                        reason: None,
-                    };
-                    self.station.submit(args).await?;
-                }
-                Ok(())
-            }
+            AssetArgsAction::Upload(args) => self.asset_upload(args).await,
+            AssetArgsAction::ComputeEvidence(args) => self.asset_compute_evidence(args).await,
+            AssetArgsAction::Check(args) => self.asset_check(args).await,
         }
     }
 
@@ -105,5 +38,93 @@ impl DfxOrbit {
             canister_agent: self.canister_agent(canister_id)?,
             logger: self.logger.clone(),
         })
+    }
+
+    async fn asset_request_prepare_permission(
+        &self,
+        args: AssetReqeustPreparePermissionArgs,
+    ) -> anyhow::Result<()> {
+        let canister_id = self.canister_id(&args.canister)?;
+        let request = self
+            .request_prepare_permission(canister_id, args.title, args.summary)
+            .await?;
+        self.print_create_request_info(&request);
+
+        Ok(())
+    }
+
+    async fn asset_upload(&self, args: AssetUploadArgs) -> anyhow::Result<()> {
+        let pathbufs = self.as_path_bufs(&args.canister, &args.files)?;
+        let paths = Self::as_paths(&pathbufs);
+
+        let canister_name = args.canister;
+        let canister_id = self.canister_id(&canister_name)?;
+        let (batch_id, evidence) = self
+            .upload(canister_id, &paths, args.ignore_evidence)
+            .await?;
+
+        let result = self
+            .request_commit_batch(
+                canister_id,
+                batch_id.clone(),
+                evidence,
+                args.title,
+                args.summary,
+            )
+            .await?;
+        let request_id = result.request.id;
+
+        let files = args.files.join(" ");
+        println!(
+            "Created request to commit batches. To verify the batch against local files, run:"
+        );
+        println!("dfx-orbit asset check {canister_name} {request_id} {batch_id} {files}");
+
+        Ok(())
+    }
+
+    async fn asset_compute_evidence(
+        &mut self,
+        args: AssetComputeEvidenceArgs,
+    ) -> Result<(), anyhow::Error> {
+        let pathbufs = self.as_path_bufs(&args.canister, &args.files)?;
+        let paths = Self::as_paths(&pathbufs);
+
+        let canister_id = self.canister_id(&args.canister)?;
+        let asset_agent = self.asset_agent(canister_id)?;
+
+        let evidence = asset_agent.compute_evidence(&paths).await?;
+        println!("{evidence}");
+        Ok(())
+    }
+
+    async fn asset_check(&mut self, args: AssetCheckArgs) -> Result<(), anyhow::Error> {
+        let pathbufs = self.as_path_bufs(&args.canister, &args.files)?;
+        let paths = Self::as_paths(&pathbufs);
+
+        let canister_id = self.canister_id(&args.canister)?;
+        let asset_agent = self.asset_agent(canister_id)?;
+
+        let evidence = asset_agent.compute_evidence(&paths).await?;
+        self.check_evidence(
+            canister_id,
+            args.request_id.clone(),
+            args.batch_id,
+            evidence,
+        )
+        .await?;
+
+        println!("Local evidence matches expected arguments");
+
+        if args.then_approve {
+            dfx_core::cli::ask_for_consent("Do you want to approve the request?")?;
+            let args = SubmitRequestApprovalInput {
+                decision: RequestApprovalStatusDTO::Approved,
+                request_id: args.request_id,
+                reason: None,
+            };
+            self.station.submit(args).await?;
+        }
+        Ok(())
     }
 }
