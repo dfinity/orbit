412f6d160f44c570b84c06fe3a0d8f27284d271d
Martin Raszyk <martin.raszyk@dfinity.org>
2024-09-13 11:52:38 +0200

    export POCKET_IC_BIN

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -33,6 +33,7 @@ echo "PocketIC download starting"
 curl -sLO https://github.com/dfinity/pocketic/releases/download/6.0.0/pocket-ic-x86_64-$PLATFORM.gz || exit 1
 gzip -df pocket-ic-x86_64-$PLATFORM.gz
 mv pocket-ic-x86_64-$PLATFORM pocket-ic
+export POCKET_IC_BIN="$(pwd)/pocket-ic"
 chmod +x pocket-ic
 echo "PocketIC download completed"
 cd ../..
