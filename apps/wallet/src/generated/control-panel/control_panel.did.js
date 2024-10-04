0e485ec51852afef474af68c2a7db8e265706181
Martin Raszyk <martin.raszyk@dfinity.org>
2024-10-04 10:55:18 +0200

    dfx generate

--- apps/wallet/src/generated/control-panel/control_panel.did.js
+++ apps/wallet/src/generated/control-panel/control_panel.did.js
@@ -252,11 +252,6 @@ export const idlFactory = ({ IDL }) => {
     'Ok' : IDL.Null,
     'Err' : ApiError,
   });
-  const WasmModuleExtraChunks = IDL.Record({
-    'wasm_module_hash' : IDL.Vec(IDL.Nat8),
-    'chunk_hashes_list' : IDL.Vec(IDL.Vec(IDL.Nat8)),
-    'store_canister' : IDL.Principal,
-  });
   const UploadCanisterModulesInput = IDL.Record({
     'station_wasm_module_extra_chunks' : IDL.Opt(
       IDL.Opt(WasmModuleExtraChunks)
