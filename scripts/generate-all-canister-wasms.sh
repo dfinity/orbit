63e2999586e607f121d2353f2165f0c4125072c2
mraszyk <31483726+mraszyk@users.noreply.github.com>
2024-10-04 15:45:57 +0200

    chore(ci): speed up rate limiter integration tests (#373)
    This PR speeds up rate limiter integration tests (in particular,
    `test_request_count_rate_limiter`) by creating requests from within a
    canister (instead of sending them as ingress messages).

--- scripts/generate-all-canister-wasms.sh
+++ scripts/generate-all-canister-wasms.sh
@@ -9,3 +9,4 @@ cd $SCRIPT_DIR/..
 ./scripts/generate-wasm.sh control-panel
 ./scripts/generate-wasm.sh upgrader
 ./scripts/generate-wasm.sh station
+./scripts/generate-wasm.sh test_canister
--- tests/integration/src/rate_limiter.rs
+++ tests/integration/src/rate_limiter.rs
@@ -1,12 +1,12 @@
-use crate::setup::{setup_new_env, WALLET_ADMIN_USER};
-use crate::utils::{bump_time_to_avoid_ratelimit, submit_request_raw};
+use crate::setup::{create_canister, get_canister_wasm, setup_new_env, WALLET_ADMIN_USER};
+use crate::utils::{bump_time_to_avoid_ratelimit, execute_request, submit_request_raw};
 use crate::{CanisterIds, TestEnv};
 use candid::{Encode, Principal};
 use orbit_essentials::api::ApiResult;
 use pocket_ic::{update_candid_as, PocketIc};
 use station_api::{
-    AddAccountOperationInput, AllowDTO, CallExternalCanisterOperationInput, CanisterMethodDTO,
-    CreateRequestInput, MeResponse, RequestExecutionScheduleDTO, RequestOperationInput,
+    CallExternalCanisterOperationInput, CanisterMethodDTO, CreateRequestInput,
+    EditUserOperationInput, MeResponse, RequestExecutionScheduleDTO, RequestOperationInput,
 };
 
 const REQUEST_RATE_LIMITER_MAX_COUNT: usize = 2000; // 2000 requests per 5mins
@@ -18,28 +18,16 @@ fn test_request_count_rate_limiter() {
         env, canister_ids, ..
     } = setup_new_env();
 
-    let res: (ApiResult<MeResponse>,) =
-        update_candid_as(&env, canister_ids.station, WALLET_ADMIN_USER, "me", ()).unwrap();
-    let user_dto = res.0.unwrap().me;
-
-    test_rate_limit(&env, canister_ids, REQUEST_RATE_LIMITER_MAX_COUNT, |i| {
-        let default_permission = AllowDTO {
-            auth_scope: station_api::AuthScopeDTO::Restricted,
-            user_groups: vec![],
-            users: vec![user_dto.id.clone()],
+    test_rate_limit(&env, canister_ids, REQUEST_RATE_LIMITER_MAX_COUNT, |id| {
+        let edit_user_operation_input = EditUserOperationInput {
+            id,
+            name: None,
+            identities: None,
+            groups: None,
+            status: None,
+            cancel_pending_requests: None,
         };
-        let add_account_operation_input = AddAccountOperationInput {
-            name: format!("test{}", i),
-            blockchain: "icp".to_string(),
-            standard: "native".to_string(),
-            read_permission: default_permission.clone(),
-            configs_permission: default_permission.clone(),
-            transfer_permission: default_permission,
-            transfer_request_policy: None,
-            configs_request_policy: None,
-            metadata: vec![],
-        };
-        RequestOperationInput::AddAccount(add_account_operation_input)
+        RequestOperationInput::EditUser(edit_user_operation_input)
     });
 }
 
@@ -79,22 +67,80 @@ fn test_request_size_rate_limiter() {
     });
 }
 
+fn register_test_canister(env: &PocketIc, canister_ids: &CanisterIds) -> Principal {
+    let test_canister = create_canister(env, WALLET_ADMIN_USER);
+    let test_canister_wasm = get_canister_wasm("test_canister");
+    env.install_canister(
+        test_canister,
+        test_canister_wasm,
+        vec![],
+        Some(WALLET_ADMIN_USER),
+    );
+
+    // register the test canister as an alternative identity of the admin
+    let res: (ApiResult<MeResponse>,) =
+        update_candid_as(env, canister_ids.station, WALLET_ADMIN_USER, "me", ()).unwrap();
+    let admin_dto = res.0.unwrap().me;
+    let edit_user = EditUserOperationInput {
+        id: admin_dto.id,
+        name: None,
+        identities: Some(vec![WALLET_ADMIN_USER, test_canister]),
+        groups: None,
+        status: None,
+        cancel_pending_requests: None,
+    };
+    execute_request(
+        env,
+        WALLET_ADMIN_USER,
+        canister_ids.station,
+        RequestOperationInput::EditUser(edit_user),
+    )
+    .unwrap();
+
+    test_canister
+}
+
 fn test_rate_limit<F>(env: &PocketIc, canister_ids: CanisterIds, rate_limit: usize, f: F)
 where
-    F: FnOnce(usize) -> RequestOperationInput + Copy,
+    F: FnOnce(String) -> RequestOperationInput + Copy,
 {
+    let test_canister = register_test_canister(env, &canister_ids);
+
+    let res: (ApiResult<MeResponse>,) =
+        update_candid_as(env, canister_ids.station, WALLET_ADMIN_USER, "me", ()).unwrap();
+    let admin_dto = res.0.unwrap().me;
+
     for _ in 0..2 {
         bump_time_to_avoid_ratelimit(env);
-        for i in 0..(rate_limit + 1) {
-            let res = submit_request_raw(env, WALLET_ADMIN_USER, canister_ids.station, f(i));
-            if i >= rate_limit {
-                let error_message = res.unwrap().0.unwrap_err().message;
-                assert!(error_message
-                    .unwrap()
-                    .contains("The request creation has been rate-limited."));
-            } else {
-                res.unwrap().0.unwrap();
-            }
-        }
+        let create_request_input = CreateRequestInput {
+            operation: f(admin_dto.id.clone()),
+            title: None,
+            summary: None,
+            execution_plan: Some(RequestExecutionScheduleDTO::Immediate),
+        };
+        let create_request_bytes = Encode!(&create_request_input).unwrap();
+        update_candid_as::<_, ()>(
+            env,
+            test_canister,
+            WALLET_ADMIN_USER,
+            "call",
+            (
+                canister_ids.station,
+                "create_request",
+                create_request_bytes,
+                rate_limit,
+            ),
+        )
+        .unwrap();
+        let res = submit_request_raw(
+            env,
+            WALLET_ADMIN_USER,
+            canister_ids.station,
+            f(admin_dto.id.clone()),
+        );
+        let error_message = res.unwrap().0.unwrap_err().message;
+        assert!(error_message
+            .unwrap()
+            .contains("The request creation has been rate-limited."));
     }
 }
