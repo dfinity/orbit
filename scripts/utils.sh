5ef3134774f4a07c940481e4ff10f46541f97cb2
Martin Raszyk <martin.raszyk@dfinity.org>
2024-11-01 16:45:06 +0100

    utils.sh

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -24,3 +24,66 @@ print_message() {
   done <<<"$msg"
   echo "$border-"
 }
+
+function get_network() {
+  local network=${IC_NETWORK:-local}
+  echo "$network"
+}
+
+function set_network() {
+  local network=$1
+  export IC_NETWORK=$network
+}
+
+function get_replica_url() {
+  local network=$(get_network)
+  local result
+
+  # Extract the first provider or bind using jq
+  result=$(jq -r --arg network "$network" \
+    '.networks[$network] |
+    if .providers then .providers[0]
+    elif .bind then "http://" + .bind
+    else null
+    end' "$DFX_JSON_PATH")
+
+  if [ -z "$result" ] || [ "$result" == "null" ]; then
+    echo -e "\e[1;31m"
+    echo "ERROR: Replica URL not found for the network: $network"
+    echo "Please make sure the dfx.json file is correctly configured."
+    echo -e "\e[0m"
+
+    exit 1
+  else
+    echo "$result"
+  fi
+}
+
+function set_identity_pem_path() {
+  local identity_name=$1
+  local identity_store_path=${DFX_DEFAULT_IDENTITY_STORE_PATH}
+
+  if [ -z "$IDENTITY_PEM_PATH" ]; then
+    local identity_pem_path="${identity_store_path}/${identity_name}/${identity_name}.pem"
+
+    # Check if the identity pem file exists, else fallback to the id.pem filename
+    if [ ! -f "$identity_pem_path" ]; then
+      identity_pem_path="${identity_store_path}/${identity_name}/identity.pem"
+    fi
+
+    if [ ! -f "$identity_pem_path" ]; then
+      identity_pem_path="${identity_store_path}/${identity_name}/id.pem"
+    fi
+
+    export IDENTITY_PEM_PATH=$identity_pem_path
+  fi
+
+  if [ ! -f "$IDENTITY_PEM_PATH" ]; then
+    echo -e "\e[1;31m"
+    echo "ERROR: Identity PEM file not found for the identity: $identity_name"
+    echo "Please make sure the identity is available in the default identity store path: $identity_store_path"
+    echo -e "\e[0m"
+
+    exit 1
+  fi
+}
