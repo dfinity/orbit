6a49bd9e53ec9b70c17e100e47819dbd630e53e4
Martin Raszyk <martin.raszyk@dfinity.org>
2024-12-13 12:21:42 +0100

    fail upgrade with pending disaster recovery requests

--- core/upgrader/impl/src/controllers/mod.rs
+++ core/upgrader/impl/src/controllers/mod.rs
@@ -3,3 +3,18 @@ mod logs;
 
 pub use disaster_recovery::*;
 pub use logs::*;
+
+use crate::services::DISASTER_RECOVERY_SERVICE;
+use ic_cdk::post_upgrade;
+
+#[post_upgrade]
+fn post_upgrade() {
+    if !DISASTER_RECOVERY_SERVICE
+        .storage
+        .get()
+        .recovery_requests
+        .is_empty()
+    {
+        ic_cdk::trap("upgrader cannot be upgraded due to pending disaster recovery requests")
+    }
+}
