84e370e68c55b122c73dc3d0f9c6524f7489f94a
Jan Hrubes <jan.hrubes@dfinity.org>
2025-01-28 09:29:16 +0100

    address comments

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://orbitwallet.io/docs',
+  site: 'https://docs.orbitwallet.io',
   integrations: [
     starlight({
       title: 'Orbit Documentation',
