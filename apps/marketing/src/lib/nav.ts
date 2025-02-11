ae09afba0a0b76b28a167a66471f9c4624c08078
olaszakos <olaszakos@gmail.com>
2025-02-11 17:42:52 +0100

    add footer

---
+++ apps/marketing/src/lib/nav.ts
@@ -0,0 +1,74 @@
+export const headerNav = {
+  navigation: [
+    {
+      label: 'Features',
+      href: '',
+    },
+    {
+      label: 'Products',
+      href: '',
+    },
+    {
+      label: 'Clients',
+      href: '',
+    },
+    {
+      label: 'FAQ',
+      href: '',
+    },
+  ],
+};
+
+export const footerNav = {
+  navigation: [
+    {
+      label: 'Features',
+      href: '',
+    },
+    {
+      label: 'Products',
+      href: '',
+    },
+    {
+      label: 'Clients',
+      href: '',
+    },
+    {
+      label: 'FAQ',
+      href: '',
+    },
+  ],
+  orbit: [
+    {
+      label: 'Contact',
+      href: '',
+    },
+    {
+      label: 'Documentation',
+      href: '',
+    },
+  ],
+};
+
+export const footerLinks = {
+  other: [
+    {
+      label: 'Privacy & terms',
+      href: '',
+    },
+    {
+      label: 'Change log',
+      href: '',
+    },
+  ],
+  social: [
+    {
+      label: 'Twitter',
+      href: '',
+    },
+    {
+      label: 'Github',
+      href: '',
+    },
+  ],
+};
