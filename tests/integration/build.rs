6692a940931a5759ee8589c299e00b254c097aa8
olaszakos <olaszakos@gmail.com>
2025-02-21 14:01:44 +0100

    add comment to build script

--- tests/integration/build.rs
+++ tests/integration/build.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let env_vars = vec![(
-        "POCKET_IC_BIN",
-        format!("{}/pocket-ic", std::env::current_dir().unwrap().display()),
-    )];
+    let env_vars = vec![
+        // The current directory for the build script is always the root directory of the package it belongs to:
+        // https://doc.rust-lang.org/cargo/reference/build-scripts.html#inputs-to-the-build-script,
+        // which is where the `pocket-ic`` binary gets downloaded to.
+        (
+            "POCKET_IC_BIN",
+            format!("{}/pocket-ic", std::env::current_dir().unwrap().display()),
+        ),
+    ];
 
     for (key, value) in env_vars {
         println!("cargo:rustc-env={}={}", key, value);
