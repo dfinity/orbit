9acad9e6e2eb93dacfa953d757b8890dc6ec4ed1
Martin Raszyk <martin.raszyk@dfinity.org>
2025-02-27 15:21:15 +0100

    locked

--- scripts/benchmark-canister.sh
+++ scripts/benchmark-canister.sh
@@ -25,7 +25,7 @@ print_message "Benchmarking canister at $CANISTER_PATH"
 # Install canbench if not already installed
 if ! cargo install --list | grep -q canbench; then
   print_message "Installing canbench..."
-  cargo install canbench --version 0.1.8
+  cargo install canbench --version 0.1.8 --locked
 fi
 
 # Changes to the canister path
