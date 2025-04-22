894a6bfb89f30bce695394bec1e14071bc772797
olaszakos <olaszakos@gmail.com>
2025-04-22 11:43:14 +0200

    use execSync instead of spawnSync

--- tests/e2e/utils/orbit.utils.ts
+++ tests/e2e/utils/orbit.utils.ts
@@ -1,35 +1,16 @@
-import { spawnSync } from 'child_process';
+import { execSync } from 'child_process';
 
 export function publishArtifact(artifact: string) {
-  const result = spawnSync('orbit-cli', ['registry', 'publish', '--app', artifact]);
-
-  if (result.status !== 0) {
-    throw new Error(`Failed to publish artifact: ${result.stderr.toString()}`);
-  }
+  execSync(`orbit-cli registry publish --app ${artifact}`, { stdio: 'inherit' });
 }
 
 export function copyArtifact(artifact: string) {
-  spawnSync('mkdir', ['-p', 'artifacts/' + artifact]);
-
-  const result = spawnSync('cp', ['wasms/' + artifact + '.wasm.gz', 'artifacts/' + artifact]);
-
-  if (result.status !== 0) {
-    throw new Error(`Failed to copy artifact: ${result.stderr.toString()}`);
-  }
+  execSync(`mkdir -p artifacts/${artifact}`, { stdio: 'inherit' });
+  execSync(`cp wasms/${artifact}.wasm.gz artifacts/${artifact}`, { stdio: 'inherit' });
 }
 
 export function topUpAccount(icpAccountIdentifier: string, amount: number) {
-  const result = spawnSync('dfx', [
-    'ledger',
-    'transfer',
-    icpAccountIdentifier,
-    '--memo',
-    '0',
-    '--icp',
-    amount.toString(),
-  ]);
-
-  if (result.status !== 0) {
-    throw new Error(`Failed to top up account: ${result.stderr.toString()}`);
-  }
+  execSync(`dfx ledger transfer ${icpAccountIdentifier} --memo 0 --icp ${amount.toString()}`, {
+    stdio: 'inherit',
+  });
 }
